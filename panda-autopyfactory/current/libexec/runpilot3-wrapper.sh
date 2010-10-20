1225e24950de76e6799486d804e18d41c623d872
plove <plove@d4488797-78bc-619e-cc48-e4164af92aee>
2010-10-20 14:42:11 +0000

    echo args if monitoring curl command fails

--- panda-autopyfactory/current/libexec/runpilot3-wrapper.sh
+++ panda-autopyfactory/current/libexec/runpilot3-wrapper.sh
@@ -133,9 +133,10 @@ function monping() {
     echo -n 'Monitor ping: '
     curl -fksS --connect-timeout 10 --max-time 20 ${APFMON}$1/$APFFID/$APFCID/$2
     if [ $? = "0" ]; then
-      echo
+        echo
     else
-      echo $?
+        echo $?
+        echo ARGS: ${APFMON}$1/$APFFID/$APFCID/$2
     fi
 }
 
