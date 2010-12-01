4451b27debb4d64421c965a4b53f67731e65b871
plove <plove@d4488797-78bc-619e-cc48-e4164af92aee>
2010-12-01 10:22:01 +0000

    use bulk updated for stale jobs

--- panda-autopyfactory/current/bin/mon-stale.py
+++ panda-autopyfactory/current/bin/mon-stale.py
@@ -9,14 +9,22 @@ Cronjob on condor host
 
 import csv
 import commands
+import itertools
 import logging
 import pycurl
 import sys
 import StringIO
 import time
+from itertools import izip, chain, repeat
 from optparse import OptionParser
+try:
+    import json as json
+except ImportError, err:
+    import simplejson as json
+
 _THISFID = 'peter-UK-devel'
 _BASEURL = 'http://py-dev.lancs.ac.uk/mon/'
+# url to report awol jobs
 _AWOLURL = _BASEURL + 'awol/'
 # url to retrieve old stale jobs
 _OLDURL = _BASEURL + 'old/' + _THISFID
@@ -35,6 +43,8 @@ class Signal:
         self.curl.setopt(pycurl.URL, self.url)
         self.curl.setopt(pycurl.POST, 1)
         self.curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+        self.curl.setopt(pycurl.CONNECTTIMEOUT, 5)
+        self.curl.setopt(pycurl.TIMEOUT, 10)
     
     def post(self, postdata):
         self.curl.setopt(pycurl.POSTFIELDS, postdata)
@@ -50,6 +60,8 @@ class Signal:
         if self.curl.getinfo(pycurl.HTTP_CODE) != 200:
             msg = "failed: %s%s" % (self.url, postdata)
             logging.debug(msg)
+            msg = "HTTP_CODE=%s" % pycurl.HTTP_CODE
+            logging.debug(msg)
             # read from start of buffer
             self.buffer.seek(0)
             logging.debug(self.buffer.read())
@@ -62,39 +74,120 @@ class Signal:
         msg = self.buffer.read()
         logging.debug(msg)
 
-def parse(line):
-    # parse condor output and return dict with keys: gk, jobstate, globusstate, cid
-    items = line.split()
-    values = {}
-    for item in items:
+    def get(self):
+        """
+        send HTTP_GET to url and return the list
+        """
+        self.curl.setopt(pycurl.URL, self.url)
         try:
-            (key, value) = item.split('=', 1)
-            values[key] = value
-        except ValueError:
-            logging.warn('Bad condor output: %s' % line)
-            continue
+            self.curl.perform()
+        except pycurl.error:
+            msg = "Problem contacting server: %s" % self.url
+            logging.error(msg)
+            return []
+        self.buffer.seek(0)
+        if self.curl.getinfo(pycurl.HTTP_CODE) != 200:
+            msg = "failed: %s" % self.url
+            logging.debug(msg)
+            # read from start of buffer
+            self.buffer.seek(0)
+            logging.debug(self.buffer.read())
+            return []
+    
+        rows = []
+        for row in csv.reader(self.buffer):
+            cid = row[0]
+            rows.append(cid)
 
-    return values
+        return rows
 
-def updatestate(state):
-    # update stale jobs via webservice
-    s = Signal(_STALEURL)
-    cid = state['cid']
-    js = state['jobstate']
-    gs = state.get('globusstate',0)
+class CondorJob:
+    def __init__(self):
+        self.json = json.JSONEncoder()
+        self.stale = []    # raw list of stale jobs
+        self.outputs = []  # raw list of condor output
+        self.awol = []     # refined list, awol only, no condor record
+        self.pending = []  # refined list, condor has record
 
-    postdata = "fid=%s&cid=%s&js=%s&gs=%s" % (_THISFID, cid, js, gs)
-    s.post(postdata)
+    def query(self):
+        """
+        get list of stale jobs from DB
+        """
+        s = Signal(_OLDURL)
+        self.stale = s.get()
+        
+    def qcondor(self):
+        """
+        take list of stale jobs from webservice and get latest status
+        create AWOL list if no job record found
+        """  
+    
+        form = '-format "cid=%s." ClusterId -format "%s " ProcId'
+        form += ' -format "jobstate=%d " JobStatus -format "globusstate=%d " GlobusStatus'
+        form += ' -format "gk=%s" MATCH_gatekeeper_url -format "-%s\\n" MATCH_queue' 
+        
+        for cid in self.stale:
+            cmd = "condor_q %s %s" % (form, cid)
+            (exitcode, output) = commands.getstatusoutput(cmd)
+            logging.debug("condor_q %s: %s" % (cid, output))
+            if output:
+                self.outputs.append(output)
+            else:
+                cmd = "condor_history -backwards %s %s" % (form, cid)
+                (exitcode, output) = commands.getstatusoutput(cmd)
+                logging.debug("condor_history %s" % output)
+                if output:
+                    self.outputs.append(output)
+                else:
+                    # cid not found by condor_q or condor_history
+                    self.awol.append(cid)
+    
+        logging.info("STALE jobs: %d" % len(self.outputs))
+        logging.info("AWOL jobs: %d" % len(self.awol))
 
-def updateawol(cid):
-    # update AWOL jobs via webservice
-    logging.warn("AWOL: %sjob/%s/%s" % (_BASEURL, _THISFID, cid))
-    s = Signal(_AWOLURL)
-    # simply tell webservice about awol jobs
-    postdata = "fid=%s&cid=%s" % (_THISFID, cid)
-    s.post(postdata)
+    def parse(self):
+        """
+        parse condor output and return dict 
+        keys: gk, jobstate, globusstate, cid
+        """
+        for line in self.outputs:
+            items = line.split()
+            values = {}
+            for item in items:
+                try:
+                    (key, value) = item.split('=', 1)
+                    values[key] = value
+                except ValueError:
+                    logging.warn('Bad condor output: %s' % line)
+                    continue
 
+            self.pending.append(values)
 
+        logging.info("PENDING jobs: %d" % len(self.pending))
+
+    def updatepending(self):
+        """
+        update pending jobs via webservice
+        """
+        if not self.pending: return
+        s = Signal(_STALEURL)
+
+        chunksize = 1000
+        ilists = izip(*[chain(self.pending, repeat(None,chunksize-1))]*chunksize)
+        for ilist in ilists:
+            jsonmsg = self.json.encode(ilist)
+            postdata = "fid=%s&data=%s" % (_THISFID, jsonmsg)
+            s.post(postdata)
+
+    def updateawol(self):
+        """
+        update AWOL jobs via webservice
+        """
+        if not self.awol: return
+        s = Signal(_AWOLURL)
+        jsonmsg = self.json.encode(self.awol)
+        postdata = "fid=%s&data=%s" % (_THISFID, jsonmsg)
+        s.post(postdata)
 
 def main():
     usage = "usage: %prog [options]"
@@ -115,71 +208,23 @@ def main():
 
     logger = logging.getLogger()
     logger.setLevel(logging._levelNames[loglevel])
-    fmt = '[PYF:%(levelname)s %(asctime)s] %(message)s'
+    fmt = '[APFMON:%(levelname)s %(asctime)s] %(message)s'
     formatter = logging.Formatter(fmt, '%T')
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    # get list of stale jobs from DB
-    pending = []
-    buffer = StringIO.StringIO()
-    curl = pycurl.Curl()
-    curl.setopt(pycurl.WRITEFUNCTION, buffer.write)
-    curl.setopt(pycurl.SSL_VERIFYPEER, 0)
-    curl.setopt(pycurl.URL, _OLDURL)
-    try:
-        curl.perform()
-    except pycurl.error:
-        msg = "Problem contacting server: %s" % _OLDURL
-        logging.error(msg)
-        return
-    buffer.seek(0)
-    if curl.getinfo(pycurl.HTTP_CODE) != 200:
-        msg = "failed: %s" % _OLDURL
-        logging.debug(msg)
-        # read from start of buffer
-        buffer.seek(0)
-        logging.debug(buffer.read())
-        return
-
-    for row in csv.reader(buffer):
-        cid = row[0]
-        pending.append(cid)
-    
-    msg = "PENDING: %d" % len(pending)
-    logging.info(msg)
-
-    form = '-format "cid=%s." ClusterId -format "%s " ProcId'
-    form += ' -format "jobstate=%d " JobStatus -format "globusstate=%d " GlobusStatus'
-    form += ' -format "gk=%s" MATCH_gatekeeper_url -format "-%s\\n" MATCH_queue' 
-    
-    outputs = []
-    awolcids = []
-    tstart = time.time()
-    for cid in pending:
-        cmd = "condor_q %s %s" % (form, cid)
-        (exitcode, output) = commands.getstatusoutput(cmd)
-        logging.debug("condor_q %s: %s" % (cid, output))
-        if output:
-            outputs.append(output)
-            state = parse(output)
-            updatestate(state)
-        else:
-            cmd = "condor_history -backwards %s %s" % (form, cid)
-            (exitcode, output) = commands.getstatusoutput(cmd)
-            logging.debug("condor_history %s: %s" % (cid, output))
-            if output:
-                outputs.append(output)
-                state = parse(output)
-                updatestate(state)
-            else:
-                # cid not found by condor_q or condor_history
-                awolcids.append(cid)
-                updateawol(cid)
-
-    logging.info("Current number of found jobs: %d" % len(outputs))
-    logging.info("Current number of AWOL jobs: %d" % len(awolcids))
+    j = CondorJob()
+    # get stale list from webservice
+    j.query()
+    # get latest condor update j.stale -> j.outputs and j.awol
+    j.qcondor()
+    # parse j.outputs -> j.pending
+    j.parse()
+    # signal updated states
+    j.updatepending()
+    # signal awol jobs
+    j.updateawol()
 
 if __name__ == "__main__":
     sys.exit(main())
