dcfa8bbd9bc819220042e234ef475777f2af2df2
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2011-06-09 15:38:20 +0000

    Classes Interfaces recovered, and some minor changes

--- panda-autopyfactory/branch/bnl-dev4/autopyfactory/factory.py
+++ panda-autopyfactory/branch/bnl-dev4/autopyfactory/factory.py
@@ -39,6 +39,7 @@ import userinterface.Client as Client
           
 class Factory:
         '''
+        -----------------------------------------------------------------------
         Class implementing the main loop. 
         The class has two main goals:
                 1. load the config files
@@ -53,6 +54,12 @@ class Factory:
         When it happens, update() does:
                 1. calculates the new list of queues from the config file
                 2. updates the WMSQueuesManager object 
+        -----------------------------------------------------------------------
+        Public Interface:
+                __init__(fcl)
+                mainLoop()
+                update()
+        -----------------------------------------------------------------------
         '''
 
         def __init__(self, fcl):
@@ -97,14 +104,15 @@ class Factory:
                         self.log.info("All Queue threads joined. Exitting.")
 
         def update(self):
-                """method to update the status of the WMSQueuesManager object.
+                '''
+                Method to update the status of the WMSQueuesManager object.
                 This method will be used every time the 
                 status of the queues changes: 
                         - at the very beginning
                         - when the config files change
                 That means this method will be invoked by the regular factory
                 main loop code or from any method capturing specific signals.
-                """
+                '''
                 newqueues = self.qcl.config.sections()
                 self.wmsmanager.update(newqueues) 
 
@@ -114,12 +122,16 @@ class Factory:
 # ==============================================================================                                
 
 class WMSQueuesManager(object):
-        """container with the list of WMSQueue objects
+        '''
+        -----------------------------------------------------------------------
+        Container with the list of WMSQueue objects.
+        -----------------------------------------------------------------------
         Public Interface:
                 __init__(factory)
                 update(newqueues)
                 join()
-        """
+        -----------------------------------------------------------------------
+        '''
         def __init__(self, factory):
                 """
                 """
@@ -131,10 +143,11 @@ class WMSQueuesManager(object):
         # --------------------
         
         def update(self, newqueues):
-                """compares the new list of queues with the current one
+                '''
+                Compares the new list of queues with the current one
                         1. creates and starts new queues if needed
                         2. stops and deletes old queues if needed
-                """
+                '''
                 currentqueues = self.queues.keys()
                 queues_to_remove, queues_to_add = \
                         self.__diff_lists(currentqueues, newqueues)
@@ -143,9 +156,10 @@ class WMSQueuesManager(object):
                 self.__refresh()
 
         def join(self):
-                """joins all WMSQueue objects
+                '''
+                Joins all WMSQueue objects
                 QUESTION: should the queues also be removed from self.queues ?
-                """
+                '''
                 for q in self.queues.values():
                         q.join()
         
@@ -154,38 +168,41 @@ class WMSQueuesManager(object):
         # --------------------
 
         def __addqueues(self, queues):
-                """creates new WMSQueue objects
-                """
+                '''
+                Creates new WMSQueue objects
+                '''
                 for qname in queues:
                         self.__add(qname)
 
         def __add(self, qname):
-                """creates a single new WMSQueue object
-                and starts it
-                """
+                '''
+                Creates a single new WMSQueue object and starts it
+                '''
                 qobject = WMSQueue(qname, self.factory)
                 self.queues[qname] = qobject
                 qobject.start()
                 
         def __delqueues(self, queues):
-                """deletes WMSQueue objects
-                """
+                '''
+                Deletes WMSQueue objects
+                '''
                 for qname in queues:
                         q = self.queues[qname]
                         q.join()
                         self.queues.pop(qname)
 
         def __del(self, qname):
-                """deletes a single queue object from the list
-                and stops it
-                """
+                '''
+                Deletes a single queue object from the list and stops it.
+                '''
                 qobject = self.__get(qname)
                 qname.join()
                 self.queues.pop(qname)
         
         def __refresh(self):
-                """calls method refresh() for all WMSQueue objects
-                """
+                '''
+                Calls method refresh() for all WMSQueue objects
+                '''
                 for q in self.queues.values():
                         q.refresh()
 
@@ -194,8 +211,9 @@ class WMSQueuesManager(object):
         # --------------------
 
         def __diff_lists(self, l1, l2):
-                """ancilla method to calculate diff between two lists
-                """
+                '''
+                Ancilla method to calculate diff between two lists
+                '''
                 d1 = [i for i in l1 if not i in l2]
                 d2 = [i for i in l2 if not i in l1]
                 return d1, d2
@@ -203,7 +221,12 @@ class WMSQueuesManager(object):
 
 class WMSQueue(threading.Thread):
         '''
+        -----------------------------------------------------------------------
         Encapsulates all the functionality related to servicing each queue (i.e. siteid, i.e. site).
+        -----------------------------------------------------------------------
+        Public Interface:
+                The class is inherited from Thread, so it has the same public interface.
+        -----------------------------------------------------------------------
         '''
         
         def __init__(self, siteid, factory):
@@ -229,14 +252,13 @@ class WMSQueue(threading.Thread):
                 self.sleep = int(self.fcl.config.get("Factory", "sleep"))
                 self.cyclesrun = 0
                 
-                # Handle sched plugin
-                self.scheduler = self.__getscheduler()
+                ### # Handle sched plugin
+                ### self.scheduler = self.__getscheduler()
 
-                # Handle status and submit batch plugins. 
-                self.batchstatus = self.__getbatchstatusplugin()
-                self.wmsstatus = self.__getwmsstatusplugin()
-                self.batchsubmit = self.__getbatchsubmitplugin()
-
+                ### # Handle status and submit batch plugins. 
+                ### self.batchstatus = self.__getbatchstatusplugin()
+                ### self.wmsstatus = self.__getwmsstatusplugin()
+                ### self.batchsubmit = self.__getbatchsubmitplugin()
 
                 # Handle sched plugin
                 self.scheduler = self.__getplugin('sched')
@@ -247,8 +269,8 @@ class WMSQueue(threading.Thread):
                 self.batchsubmit = self.__getplugin('batchsubmit')
 
         def __getplugin(self, action):
-                """
-                generic private method to find out the specific plugin
+                '''
+                Generic private method to find out the specific plugin
                 to be used for this queue, depending on the action.
                 Action can be:
                         - sched
@@ -269,7 +291,7 @@ class WMSQueue(threading.Thread):
                            The name of the class is supposed to have format
                            <prefix>Plugin
                            For example: SchedPlugin(), BatchStatusPlugin()
-                """
+                '''
 
                 plugin_prefixes = {
                         'sched' : 'Sched',
@@ -293,53 +315,53 @@ class WMSQueue(threading.Thread):
                 plugin_class = '%sPlugin' %plugin_prefix
                 return gettattr(plugin_module, plugin_class)()
 
-        def __getscheduler(self): 
-                """
-                private method to find out the specific Sched Plugin
-                to be used for this queue.
-                """
-                schedclass = self.qcl.config.get(self.siteid, "schedplugin")
-                self.log.debug("[%s] Attempting to import derived classname: \
-                                autopyfactory.plugins.%sSchedPlugin.%sSchedPlugin"
-                                % (self.siteid,schedclass,schedclass))                                
-                _temp = __import__("autopyfactory.plugins.%sSchedPlugin" % (schedclass), 
-                                                                 fromlist=["%sSchedPlugin" % schedclass])
-                SchedPlugin = _temp.SchedPlugin
-                # all plugins have a class SchedPlugin
-                return SchedPlugin()
+        ###def __getscheduler(self): 
+        ###        """
+        ###        private method to find out the specific Sched Plugin
+        ###        to be used for this queue.
+        ###        """
+        ###        schedclass = self.qcl.config.get(self.siteid, "schedplugin")
+        ###        self.log.debug("[%s] Attempting to import derived classname: \
+        ###                        autopyfactory.plugins.%sSchedPlugin.%sSchedPlugin"
+        ###                        % (self.siteid,schedclass,schedclass))                                
+        ###        _temp = __import__("autopyfactory.plugins.%sSchedPlugin" % (schedclass), 
+        ###                                                         fromlist=["%sSchedPlugin" % schedclass])
+        ###        SchedPlugin = _temp.SchedPlugin
+        ###        # all plugins have a class SchedPlugin
+        ###        return SchedPlugin()
 
-        def __getbatchstatusplugin(self):
-                """
-                private method to find out the specific Batch Status Plugin
-                to be used for this queue.
-                """
+        ###def __getbatchstatusplugin(self):
+        ###        """
+        ###        private method to find out the specific Batch Status Plugin
+        ###        to be used for this queue.
+        ###        """
 
-                batchstatclass = self.qcl.config.get(self.siteid, "batchplugin")
+        ###        batchstatclass = self.qcl.config.get(self.siteid, "batchplugin")
 
-                _temp =  __import__("autopyfactory.plugins.%sBatchPlugin" % batchstatclass, 
-                                                                 fromlist=["%sBatchStatusPlugin" %batchstatclass ])
-                BatchStatusPlugin = _temp.BatchStatusPlugin
-                return BatchStatusPlugin(self)
-                
-        def __getbatchsubmitplugin(self):
-                """
-                private method to find out the specific Batch Submit Plugin
-                to be used for this queue.
-                """
+        ###        _temp =  __import__("autopyfactory.plugins.%sBatchPlugin" % batchstatclass, 
+        ###                                                         fromlist=["%sBatchStatusPlugin" %batchstatclass ])
+        ###        BatchStatusPlugin = _temp.BatchStatusPlugin
+        ###        return BatchStatusPlugin(self)
+        ###        
+        ###def __getbatchsubmitplugin(self):
+        ###        """
+        ###        private method to find out the specific Batch Submit Plugin
+        ###        to be used for this queue.
+        ###        """
 
-                batchsubmitclass = self.qcl.config.get(self.siteid, "batchplugin")
+        ###        batchsubmitclass = self.qcl.config.get(self.siteid, "batchplugin")
 
-                _temp =  __import__("autopyfactory.plugins.%sBatchSubmitPlugin" % batchsubmitclass, 
-                                                                 fromlist=["%sBatchSubmitPlugin" %batchsubmitclass ])
-                BatchSubmitPlugin = _temp.BatchSubmitPlugin
-                self.log.debug("[%s] WMSQueue initialization done." % self.siteid)
-                return BatchSubmitPlugin(self)
+        ###        _temp =  __import__("autopyfactory.plugins.%sBatchSubmitPlugin" % batchsubmitclass, 
+        ###                                                         fromlist=["%sBatchSubmitPlugin" %batchsubmitclass ])
+        ###        BatchSubmitPlugin = _temp.BatchSubmitPlugin
+        ###        self.log.debug("[%s] WMSQueue initialization done." % self.siteid)
+        ###        return BatchSubmitPlugin(self)
 
 
         def run(self):
                 '''
                 Method called by thread.start()
-                Main functional loop of this Queue. 
+                Main functional loop of this WMSQueue. 
                 '''        
                 while not self.stopevent.isSet():
                         self.log.debug("[%s] Would be grabbing Batch info relevant to this queue." % self.siteid)
@@ -349,10 +371,10 @@ class WMSQueue(threading.Thread):
                         
                         self.log.debug("[%s] Would be calculating number to submit."% self.siteid)
                         # calculate number to submit
-                        #nsub = self.scheduler.calcSubmitNum()
+                        nsub = self.scheduler.calcSubmitNum(self.status)
                         # submit using this number
                         self.log.debug("[%s] Would be submitting jobs for this queue."% self.siteid)
-                        #self.submitPilots(nsub)
+                        self.batchsubmit.submit(nsub)
                         # Exit loop if desired number of cycles is reached...  
                         self.log.debug("[%s] Checking to see how many cycles to run."% self.siteid)
                         if self.cycles and self.cyclesrun >= self.cycles:
@@ -364,15 +386,16 @@ class WMSQueue(threading.Thread):
                         time.sleep(self.sleep)
 
         def refresh(self):
-                """ method to reload, when requested, the config file
-                """
+                '''
+                Method to reload, when requested, the config file
+                '''
                 pass 
                 # TO BE IMPLEMENTED
                           
         def join(self,timeout=None):
-                """
+                '''
                 Stop the thread. Overriding this method required to handle Ctrl-C from console.
-                """
+                '''
                 self.stopevent.set()
                 self.log.debug('[%s] Stopping thread...' % self.siteid )
                 threading.Thread.join(self, timeout)
@@ -383,9 +406,11 @@ class WMSQueue(threading.Thread):
 # ==============================================================================                                
 
 class Status(object):
-        """ancilla class to collect all relevant status variables
-        in a single object.
-        """
+        '''
+        -----------------------------------------------------------------------
+        Ancilla class to collect all relevant status variables in a single object.
+        -----------------------------------------------------------------------
+        '''
         def __init__(self):
                 # I use None instead of 0 to distinguish between
                 #       - value has been provided and it is 0
@@ -398,7 +423,9 @@ class Status(object):
 
 class SchedInterface(object):
         '''
-        Calculates the number of jobs to submit for a queue. 
+        -----------------------------------------------------------------------
+        Calculates the number of jobs to be submitted for a given queue. 
+        -----------------------------------------------------------------------
         '''
         def calcSubmitNum(self, status):
                 '''
@@ -415,6 +442,55 @@ class SchedInterface(object):
                 'holding' : 3,
                 'running': 18,
                 'transferring': 38},
+                '''
+                raise NotImplementedError
+
+
+class BatchStatusInterface(object):
+        '''
+        -----------------------------------------------------------------------
+        Interacts with the underlying batch system to get job status. 
+        Instantiated at the Factory level. 
+        Should return information about number of jobs currently on the desired queue. 
+        -----------------------------------------------------------------------
+        Public Interface:
+                getInfo(queue)
+                getJobInfo(queue) 
+        -----------------------------------------------------------------------
+        '''
+        def getInfo(self, queue):
+                '''
+                Returns aggregate info about jobs on queue in batch system. 
+                '''
+                raise NotImplementedError
+
+        def getJobInfo(self, queue):
+                '''
+                Returns a list of JobStatus objects, one for each job. 
+                '''
+                raise NotImplementedError
+
+
+class WMSStatusInterface(object):
+        '''
+        '''
+        pass
+
+
+
+class BatchSubmitInterface(object):
+        '''
+        -----------------------------------------------------------------------
+        Interacts with underlying batch system to submit jobs. 
+        It should be instantiated one per queue. 
+        -----------------------------------------------------------------------
+        Public Interface:
+                submitPilots(number)
+        -----------------------------------------------------------------------
+        '''
+        def submitPilots(self, number):
+                '''
+                Method to submit pilots 
                 '''
                 raise NotImplementedError
 
