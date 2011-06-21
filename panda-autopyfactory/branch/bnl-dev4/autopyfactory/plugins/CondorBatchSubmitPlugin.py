324de6c4dee637a3f1bd824ddc16a0ea7a91bd28
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2011-06-21 19:39:57 +0000

    minor changes

--- panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/CondorBatchSubmitPlugin.py
+++ panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/CondorBatchSubmitPlugin.py
@@ -74,55 +74,55 @@ class BatchSubmitPlugin(BatchSubmitInterface):
                 self.JSD.add("notify_user=%s" % self.fcl.config.get('Factory', 'factoryOwner'))
                 self.JSD.add("universe=grid")
 
-                # Here we insert the switch for CREAM CEs. This is rather a hack for now, but will
-                # improve once multiple backends are supported properly
-                if self.config.queues[self.queue]['_isCream']:
-                        self.JSD.add("grid_resource=cream %s:%d/ce-cream/services/CREAM2 %s %s" % (
-                                 self.config.queues[self.queue]['_creamHost'], self.config.queues[self.queue]['_creamPort'], 
-                                 self.config.queues[self.queue]['_creamBatchSys'], self.config.queues[self.queue]['localqueue']))
-                else:
-                        # GRAM resource
-                        self.JSD.add("grid_resource=gt2 %s" % self.config.queues[self.queue]['queue'])
-                        self.JSD.add("globusrsl=(queue=%s)(jobtype=single)" % self.config.queues[self.queue]['localqueue'])
-                # Probably not so helpful to set these in the JDL
-                #if self.config.queues[self.queue]['memory'] != None:
-                #        self.JSD.add("(maxMemory=%d)" % self.config.queues[self.queue]['memory'],)
-                #if self.config.queues[self.queue]['wallClock'] != None:
-                #        self.JSD.add("(maxWallTime=%d)" % self.config.queues[self.queue]['wallClock'],)
-                ##print >>JDL
-                #self.JSD.add('+MATCH_gatekeeper_url="%s"' % self.config.queues[self.queue]['queue'])
-                #self.JSD.add('+MATCH_queue="%s"' % self.config.queues[self.queue]['localqueue'])
-                self.JSD.add("x509userproxy=%s" % self.config.queues[self.queue]['gridProxy'])
-                self.JSD.add('periodic_hold=GlobusResourceUnavailableTime =!= UNDEFINED &&(CurrentTime-GlobusResourceUnavailableTime>30)')
-                self.JSD.add('periodic_remove = (JobStatus == 5 && (CurrentTime - EnteredCurrentStatus) > 3600) || (JobStatus == 1 && globusstatus =!= 1 && (CurrentTime - EnteredCurrentStatus) > 86400)')
-                # In job environment correct GTAG to URL for logs, JSID should be factoryId
-                self.JSD.add('environment = "PANDA_JSID=%s' % self.fcl.config.get('Factory', 'factoryId'),)
-                self.JSD.add('GTAG=%s/$(Cluster).$(Process).out' % self.logUrl,)
-                self.JSD.add('APFCID=$(Cluster).$(Process)',)
-                self.JSD.add('APFFID=%s' % self.fcl.config.get('Factory', 'factoryId'),)
-                if isinstance(self.mon, Monitor):
-                        self.JSD.add('APFMON=%s' % self.fcl.config.get('Factory', 'monitorURL'),)
-                self.JSD.add('FACTORYQUEUE=%s' % self.queue,)
-                if self.config.queues[self.queue]['user'] != None:
-                        self.JSD.add('FACTORYUSER=%s' % self.config.queues[self.queue]['user'],)
-                if self.config.queues[self.queue]['environ'] != None and self.config.queues[self.queue]['environ'] != '':
-                        self.JSD.add(self.config.queues[self.queue]['environ'],)
-                self.JSD.add('"')
-                self.JSD.add("arguments = -s %s -h %s" % (self.config.queues[self.queue]['siteid'], self.config.queues[self.queue]['nickname']),)
-                self.JSD.add("-p %d -w %s" % (self.config.queues[self.queue]['port'], self.config.queues[self.queue]['server']),)
-                if self.config.queues[self.queue]['jobRecovery'] == False:
-                        self.JSD.add(" -j false",)
-                if self.config.queues[self.queue]['memory'] != None:
-                        self.JSD.add(" -k %d" % self.config.queues[self.queue]['memory'],)
-                if self.config.queues[self.queue]['user'] != None:
-                        self.JSD.add(" -u %s" % self.config.queues[self.queue]['user'],)
-                if self.config.queues[self.queue]['group'] != None:
-                        self.JSD.add(" -v %s" % self.config.queues[self.queue]['group'],)
-                if self.config.queues[self.queue]['country'] != None:
-                        self.JSD.add(" -o %s" % self.config.queues[self.queue]['country'],)
-                if self.config.queues[self.queue]['allowothercountry'] == True:
-                        self.JSD.add(" -A True",)
-                #print >>JDL
+                ###  # Here we insert the switch for CREAM CEs. This is rather a hack for now, but will
+                ###  # improve once multiple backends are supported properly
+                ###  if self.config.queues[self.queue]['_isCream']:
+                ###          self.JSD.add("grid_resource=cream %s:%d/ce-cream/services/CREAM2 %s %s" % (
+                ###                   self.config.queues[self.queue]['_creamHost'], self.config.queues[self.queue]['_creamPort'], 
+                ###                   self.config.queues[self.queue]['_creamBatchSys'], self.config.queues[self.queue]['localqueue']))
+                ###  else:
+                ###          # GRAM resource
+                ###          self.JSD.add("grid_resource=gt2 %s" % self.config.queues[self.queue]['queue'])
+                ###          self.JSD.add("globusrsl=(queue=%s)(jobtype=single)" % self.config.queues[self.queue]['localqueue'])
+                ###  # Probably not so helpful to set these in the JDL
+                ###  #if self.config.queues[self.queue]['memory'] != None:
+                ###  #        self.JSD.add("(maxMemory=%d)" % self.config.queues[self.queue]['memory'],)
+                ###  #if self.config.queues[self.queue]['wallClock'] != None:
+                ###  #        self.JSD.add("(maxWallTime=%d)" % self.config.queues[self.queue]['wallClock'],)
+                ###  ##print >>JDL
+                ###  #self.JSD.add('+MATCH_gatekeeper_url="%s"' % self.config.queues[self.queue]['queue'])
+                ###  #self.JSD.add('+MATCH_queue="%s"' % self.config.queues[self.queue]['localqueue'])
+                self.JSD.add("x509userproxy=%s" % self.qcl.config.get(self.queue, 'gridProxy'))
+                ###  self.JSD.add('periodic_hold=GlobusResourceUnavailableTime =!= UNDEFINED &&(CurrentTime-GlobusResourceUnavailableTime>30)')
+                ###  self.JSD.add('periodic_remove = (JobStatus == 5 && (CurrentTime - EnteredCurrentStatus) > 3600) || (JobStatus == 1 && globusstatus =!= 1 && (CurrentTime - EnteredCurrentStatus) > 86400)')
+                ###  # In job environment correct GTAG to URL for logs, JSID should be factoryId
+                ###  self.JSD.add('environment = "PANDA_JSID=%s' % self.fcl.config.get('Factory', 'factoryId'),)
+                ###  self.JSD.add('GTAG=%s/$(Cluster).$(Process).out' % self.logUrl,)
+                ###  self.JSD.add('APFCID=$(Cluster).$(Process)',)
+                ###  self.JSD.add('APFFID=%s' % self.fcl.config.get('Factory', 'factoryId'),)
+                ###  if isinstance(self.mon, Monitor):
+                ###          self.JSD.add('APFMON=%s' % self.fcl.config.get('Factory', 'monitorURL'),)
+                ###  self.JSD.add('FACTORYQUEUE=%s' % self.queue,)
+                ###  if self.config.queues[self.queue]['user'] != None:
+                ###          self.JSD.add('FACTORYUSER=%s' % self.config.queues[self.queue]['user'],)
+                ###  if self.config.queues[self.queue]['environ'] != None and self.config.queues[self.queue]['environ'] != '':
+                ###          self.JSD.add(self.config.queues[self.queue]['environ'],)
+                ###  self.JSD.add('"')
+                ###  self.JSD.add("arguments = -s %s -h %s" % (self.config.queues[self.queue]['siteid'], self.config.queues[self.queue]['nickname']),)
+                ###  self.JSD.add("-p %d -w %s" % (self.config.queues[self.queue]['port'], self.config.queues[self.queue]['server']),)
+                ###  if self.config.queues[self.queue]['jobRecovery'] == False:
+                ###          self.JSD.add(" -j false",)
+                ###  if self.config.queues[self.queue]['memory'] != None:
+                ###          self.JSD.add(" -k %d" % self.config.queues[self.queue]['memory'],)
+                ###  if self.config.queues[self.queue]['user'] != None:
+                ###          self.JSD.add(" -u %s" % self.config.queues[self.queue]['user'],)
+                ###  if self.config.queues[self.queue]['group'] != None:
+                ###          self.JSD.add(" -v %s" % self.config.queues[self.queue]['group'],)
+                ###  if self.config.queues[self.queue]['country'] != None:
+                ###          self.JSD.add(" -o %s" % self.config.queues[self.queue]['country'],)
+                ###  if self.config.queues[self.queue]['allowothercountry'] == True:
+                ###          self.JSD.add(" -A True",)
+                ###  #print >>JDL
                 self.JSD.add("queue %d" % self.nbpilots)
                 #return 0
 
@@ -148,23 +148,7 @@ class BatchSubmitPlugin(BatchSubmitInterface):
                 Submit pilots
                 '''
 
-                ###def _condorPilotSubmit(self, queue, cycleNumber=0, pilotNumber=1):
-                ###     if error != 0:
-                ###             self.log.error('Cannot submit pilots for %s', gatekeeper)
-                ###             return
-                ###     if not self.dryRun:
-                ###             (exitStatus, output) = commands.getstatusoutput('condor_submit -verbose ' + self.jdlFile)
-                ###             if exitStatus != 0:
-                ###                     self.log.error('condor_submit command for %s failed (status %d): %s', queue, exitStatus, output)
-                ###             else:
-                ###                     self.log.debug('condor_submit command for %s succeeded', queue)
-                ###                     if isinstance(self.mon, Monitor):
-                ###                             nick = self.config.queues[self.queue]['nickname']
-                ###                             label = queue
-                ###                             self.mon.notify(nick, label, output)
-                ###     else:
-                ###             self.log.debug('Dry run mode - pilot submission supressed.')
-
+                self.dryRun = self.fcl.config.get('Factory', 'dryRun')
                 if not self.dryRun:
                         (exitStatus, output) = commands.getstatusoutput('condor_submit -verbose ' + self.jdlFile)
                         if exitStatus != 0:
@@ -172,7 +156,7 @@ class BatchSubmitPlugin(BatchSubmitInterface):
                         else:
                                 self.log.debug('condor_submit command for %s succeeded', self.queue)
                                 if isinstance(self.mon, Monitor):
-                                        nick = self.config.queues[self.queue]['nickname']
+                                        nick = self.qcl.config.get(self.queue, 'nickname')
                                         label = self.queue
                                         self.mon.notify(nick, label, output)
                 else:
