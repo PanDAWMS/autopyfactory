ad28caa74777f832c94f6a2f5cc45686389dddde
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2011-07-06 20:19:39 +0000

    making the Trivial plugin a little bit less trivial

--- panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/TrivialSchedPlugin.py
+++ panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/TrivialSchedPlugin.py
@@ -14,9 +14,27 @@ class SchedPlugin(SchedInterface):
                 self.log.debug("TrivialSchedPlugin initializing...")
 
         def calcSubmitNum(self, status):
-                """ always return 0
+                """ 
+                is number of actived jobs == 0 ?
+                        yes -> return 0
+                        no ->
+                                is number of activated > number of idle+running pilots?
+                                        yes -> return nbjobs - nbpilots
+                                        no -> return 0
                 """
-                return 0
 
-
-                
+                if not status:
+                        return 0
+                else:
+                        nbjobs = status.jobs.get('activated', 0)
+                        nbpilots = status.batch.get('1', 0) + status.batch.get('2', 0)
+                        # note: the following if-else algorithm can be written
+                        #       in a simpler way, but in this way is easier to 
+                        #       read and to understand what it does and why.
+                        if nbjobs == 0:
+                                return 0
+                        else:
+                                if nbjobs > nbpilots:
+                                        return nbjobs - nbpilots
+                                else:
+                                        return 0
