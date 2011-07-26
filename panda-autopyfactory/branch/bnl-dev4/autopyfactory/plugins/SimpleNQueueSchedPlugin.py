a87be8429df744e503b59235ea05c8a83bcded56
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2011-07-26 15:49:21 +0000

    creating sched pluging that makes use of nqueue

---
+++ panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/SimpleNQueueSchedPlugin.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+#
+
+from autopyfactory.factory import SchedInterface
+import logging
+
+__author__ = "John Hover, Jose Caballero"
+__copyright__ = "2011 John Hover, Jose Caballero"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "2.0.0"
+__maintainer__ = "Jose Caballero"
+__email__ = "jcaballero@bnl.gov,jhover@bnl.gov"
+__status__ = "Production"
+
+class SchedPlugin(SchedInterface):
+        
+        def __init__(self, wmsqueue):
+                
+                self.log = logging.getLogger("main.schedplugin[%s]" %wmsqueue.siteid)
+                self.wmsqueue = wmsqueue
+                self.log.info("SchedPlugin: Object initialized.")
+
+        def calcSubmitNum(self, status):
+                """ 
+                is nqueue > number of idle?
+                        yes -> return nqueue - nbpilots
+                        no -> return 0
+
+                This version makes use of config variable maxPilotsPerCycle.
+                """
+
+                self.log.debug('calcSubmitNum: Starting with input %s' %status)
+
+                if not status:
+                        out = 0
+                else:
+                        nqueue = self.wmsqueue.qcl.getint(self.wmsqueue.siteid, 'nqueue')
+                        nbpilots = status.batch.get('1', 0)
+                        # '1' means pilots in Idle status
+
+                        if nqueue > nbpilots:
+                                out = nqueue - nbpilots
+                        else:
+                                out = 0
+                
+                # check if the config file has attribute maxPilotsPerCycle
+                if self.wmsqueue.qcl.has_option(self.wmsqueue.siteid, 'maxPilotsPerCycle'):
+                        maxPilotsPerCycle = self.wmsqueue.qcl.getint(self.wmsqueue.siteid, 'maxPilotsPerCycle')
+                        out = min(out, maxPilotsPerCycle)
+
+                self.log.debug('calcSubmitNum: Leaving returning %s' %out)
+                return out
