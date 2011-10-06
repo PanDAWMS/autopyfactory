6c47e85ce06bdbd350957d2003890a14c42078e2
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2011-10-06 14:59:30 +0000

    using proxy only when defined

--- panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/CondorLocalBatchSubmitPlugin.py
+++ panda-autopyfactory/branch/bnl-dev4/autopyfactory/plugins/CondorLocalBatchSubmitPlugin.py
@@ -116,9 +116,9 @@ class BatchSubmitPlugin(BatchSubmitInterface):
                 self.JSD.add('+MATCH_APF_QUEUE="%s"' % self.queue)
 
                 # -- proxy path --
-                #self.JSD.add("x509userproxy=%s" % self.qcl.get(self.queue, 'gridProxy'))
-                x509userproxy = self.factory.proxymanager.getProxyPath(self.qcl.get(self.queue,'proxy'))
-                self.JSD.add("x509userproxy=%s" % x509userproxy)
+                if self.qcl.has_option(self.queue, 'proxy'):
+                        x509userproxy = self.factory.proxymanager.getProxyPath(self.qcl.get(self.queue,'proxy'))
+                        self.JSD.add("x509userproxy=%s" % x509userproxy)
 
                 # -- stuffs -- 
                 self.JSD.add('periodic_remove = (JobStatus == 5 && (CurrentTime - EnteredCurrentStatus) > 3600) || (JobStatus == 1 && globusstatus =!= 1 && (CurrentTime - EnteredCurrentStatus) > 86400)')
