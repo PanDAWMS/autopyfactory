c7d87cdf9c1cba7863660eba063424b9153a519d
jhover <jhover@d4488797-78bc-619e-cc48-e4164af92aee>
2011-10-26 19:39:11 +0000

    Adjusting RPM script.

--- panda-autopyfactory/branch/bnl-dev5/misc/rpm-pre.sh
+++ panda-autopyfactory/branch/bnl-dev5/misc/rpm-pre.sh
@@ -2,4 +2,9 @@
 #if [ -f /etc/apf/factory.conf ] ; then
 #	cp -f /etc/apf/factory.conf /etc/apf/factory.conf.bak
 #fi
-/usr/sbin/useradd --comment "AutoPyFactory service account" --shell /sbin/nologin apf 
+
+if id apf > /dev/null 2>&1; then
+	: # do nothing
+else
+    /usr/sbin/useradd --comment "AutoPyFactory service account" --shell /sbin/nologin apf
+fi 
