d75a85f987b314f92da95b3f9e9c8537e212db81
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-02-16 21:17:19 +0000

    first prototype for SCConfigPlugin.py

---
+++ panda-autopyfactory/branch/bnl-dev5/autopyfactory/plugins/SCConfigPlugin.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+
+import logging
+import time
+import traceback
+
+from urllib import urlopen
+
+#from autopyfactory.factory import WMSStatusInterface
+#from autopyfactory.factory import WMSStatusInfo
+#from autopyfactory.info import InfoContainer
+import autopyfactory.utils as utils
+import userinterface.Client as Client
+
+__author__ = "John Hover, Jose Caballero"
+__copyright__ = "2011 John Hover, Jose Caballero"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "2.0.0"
+__maintainer__ = "Jose Caballero"
+__email__ = "jcaballero@bnl.gov,jhover@bnl.gov"
+__status__ = "Production"
+
+
+class SCConfigPlugin(ConfigInterface):
+    '''
+    -----------------------------------------------------------------------
+    -----------------------------------------------------------------------
+    Public Interface:
+            the interfaces inherited from ConfigInterface
+    -----------------------------------------------------------------------
+    '''
+
+
+    def __init__(self, apfqueue):
+        self._valid = True
+        try:
+
+            self.apfqname = apfqueue.apfqname
+            self.log = logging.getLogger("main.scconfigplugin[%s]" %apfqueue.apfqname)
+            self.log.debug("scconfigplugin: Initializing object...")
+
+            self.qcl = apfqueue.factory.qcl
+            self.batchqueue = self.qcl.get(self.apfqname, 'batchqueue')
+
+            # temporary draft solution
+            self.gridresource = None 
+            self.queue = None 
+            self._getschedconfig() 
+
+            self.log.info('scconfigplugin: Object initialized.')
+        except:
+            self._valid = False
+
+    def valid(self):
+        return self._valid
+
+    def _getschedconfig(self):
+        ''' 
+        ''' 
+        try:
+                import json as json
+        except ImportError, err:
+                log.warning('_getschedconfig: json package not installed. Trying to import simplejson as json')
+            import simplejson as json
+
+
+        try:
+                url = 'http://pandaserver.cern.ch:25080/cache/schedconfig/%s.factory.json' % self.batchqueue
+                handle = urlopen(url)
+                jsonData = json.load(handle, 'utf-8')
+                handle.close()
+                self.log.info('_getschedconfig: JSON returned: %s' % jsonData)
+                factoryData = {}
+                # json always gives back unicode strings (eh?) - convert unicode to utf-8
+                for k, v in jsonData.iteritems():
+                        if isinstance(k, unicode):
+                                k = k.encode('utf-8')
+                        if isinstance(v, unicode):
+                                v = v.encode('utf-8')
+                        factoryData[k] = v
+                        
+                        if k == 'localqueue':
+                                self.queue = v
+                        if k == 'queue':
+                                self.gridresource = v
+
+                self.log.debug('_getschedconfig: Converted to: %s' % factoryData)
+        except ValueError, err:
+                self.log.error('_getschedconfig: %s for queue %s, downloading from %s' % (err, self.batchqueue, url))
+        except IOError, (errno, errmsg):
+                self.log.error('_getschedconfig: %s for queue %s, downloading from %s' % (errmsg, self.batchqueue, url))
+
+    #def getCloudInfo(self, maxtime=0):
+    #    '''
+    #    '''
+    #
+    #    self.log.debug('scconfigplugin: Starting maxtime = %s' %maxtime)
+    #    self.log.info('scconfigplugin: Cloud has %d entries' % len(out))
