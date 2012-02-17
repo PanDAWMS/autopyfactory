dba4c3a222df7ff97db8c7141e963304d65e2be6
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-02-17 16:14:54 +0000

    bug

--- panda-autopyfactory/branch/bnl-dev5/autopyfactory/plugins/jsd.py
+++ panda-autopyfactory/branch/bnl-dev5/autopyfactory/plugins/jsd.py
@@ -18,7 +18,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import copy
 import logging
 import os
 import re
