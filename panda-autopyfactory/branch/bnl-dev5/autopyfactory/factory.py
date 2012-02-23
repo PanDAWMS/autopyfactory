fa3b5a9642fe17b020ea1b0d9044de73ca456e25
plove <plove@d4488797-78bc-619e-cc48-e4164af92aee>
2012-02-23 14:14:37 +0000

    reduce log noise

--- panda-autopyfactory/branch/bnl-dev5/autopyfactory/factory.py
+++ panda-autopyfactory/branch/bnl-dev5/autopyfactory/factory.py
@@ -232,16 +232,16 @@ Jose Caballero <jcaballero@bnl.gov>
         if not 'APF_NOSQUID' in os.environ:
             if not 'PANDA_URL_MAP' in os.environ:
                 os.environ['PANDA_URL_MAP'] = 'CERN,http://pandaserver.cern.ch:25085/server/panda,https://pandaserver.cern.ch:25443/server/panda'
-                self.log.warning('FACTORY DEBUG: Set PANDA_URL_MAP to %s' % os.environ['PANDA_URL_MAP'])
+                self.log.debug('Set PANDA_URL_MAP to %s' % os.environ['PANDA_URL_MAP'])
             else:
-                self.log.warning('FACTORY DEBUG: Found PANDA_URL_MAP set to %s. Not changed.' % os.environ['PANDA_URL_MAP'])
+                self.log.debug('Found PANDA_URL_MAP set to %s. Not changed.' % os.environ['PANDA_URL_MAP'])
             if not 'PANDA_URL' in os.environ:
                 os.environ['PANDA_URL'] = 'http://pandaserver.cern.ch:25085/server/panda'
-                self.log.warning('FACTORY DEBUG: Set PANDA_URL to %s' % os.environ['PANDA_URL'])
+                self.log.debug('Set PANDA_URL to %s' % os.environ['PANDA_URL'])
             else:
-                self.log.warning('FACTORY DEBUG: Found PANDA_URL set to %s. Not changed.' % os.environ['PANDA_URL'])
+                self.log.debug('Found PANDA_URL set to %s. Not changed.' % os.environ['PANDA_URL'])
         else:
-            self.log.warning('FACTORY DEBUG: Found APF_NOSQUID set. Not changing/setting panda client environment.')
+            self.log.debug('Found APF_NOSQUID set. Not changing/setting panda client environment.')
 
 
     def checkroot(self): 
