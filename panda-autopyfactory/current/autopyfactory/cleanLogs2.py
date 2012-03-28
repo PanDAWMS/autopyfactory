9cd8807b0d84811af12a3f9dc14800225b634893
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-03-28 00:27:49 +0000

    some experimenation

--- panda-autopyfactory/current/autopyfactory/cleanLogs2.py
+++ panda-autopyfactory/current/autopyfactory/cleanLogs2.py
@@ -207,5 +207,72 @@ class CleanLogs(threading.Thread):
         sleeptime = 24 * 60 * 60 
         time.sleep(sleeptime) 
 
-           
 
+# =============================================================
+
+
+class DirMgr(object):
+    '''
+    class to create a list of Dir objects
+    '''
+    def __init__(self, basedir):
+        self.basedir = basedir
+        self.dirs = self.getdirs() 
+
+    def getdirs(self):
+ 
+        if not os.access(self.basedir, os.F_OK):
+            self.log.warning('__getdirs: Base log directory %s does not exist - nothing to do',
+                      self.basedir)
+            self.log.warning("__getdirs: Leaving with no output.") 
+            return []
+        # else (==the base directory exists)
+        dirs = os.listdir(self.basedir)
+        # we only consider dirs looking like <logDir>/2011-08-12/
+        #   -- we need to insert <logDir> at the beginning
+        #   -- there is a file called robot.txt which does not match
+        logDirRe = re.compile(r"(\d{4})-(\d{2})-(\d{2})?$")
+        dirs = [os.path.join(self.logDir, d) for d in dirs if logDirRe.match(d)]
+      
+ 
+           
+class Dir(object):
+    '''
+    class to manage each parent directory.
+    The parent directory looks like <logDir>/2011-08-12/ 
+    '''
+
+    def __init__(self, basedir, dir):
+        '''
+        basedir is <logDir>
+        dir is like 2011-08-12
+        '''
+        self.dirRe = re.compile(r"(\d{4})-(\d{2})-(\d{2})?$")
+
+        self.basedir = basedir
+        self.dir = dir
+        self.path = os.path.join(basedir, dir)
+        self.creation_t = self.creation_t() 
+
+    def empty(self):
+        '''
+        checks if the parent directory is empty
+        '''
+        return os.listdir(self.path) == []
+
+    def creation_t(self):
+        '''
+        returns a datetime object with the creation time.
+        Creation time is calculated from the self.dir itself.
+        '''
+        match = self.dirRe.match(self.dir)
+        creation_t = datetime.date(int(match.group(1)), 
+                                   int(match.group(2)), 
+                                   int(match.group(3)))
+        return creation_t
+
+   def subdirs(self):
+        ''' 
+        returns the list of subdirs 
+        ''' 
+        return os.listdir(path)
