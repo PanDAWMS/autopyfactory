33a7e4becb480f5d10e08fe0e150ff0efb65eb16
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-03-28 15:12:42 +0000

    new classes added

--- panda-autopyfactory/current/autopyfactory/cleanLogs2.py
+++ panda-autopyfactory/current/autopyfactory/cleanLogs2.py
@@ -59,7 +59,6 @@ class CleanLogs(threading.Thread):
         while True:
             try:
                 while not self.stopevent.isSet():
-                    self.__getkeepdays()
                     self.__process()
                     self.__sleep()
             except Exception, e:
@@ -74,129 +73,29 @@ class CleanLogs(threading.Thread):
 
         self.log.debug("__process: Starting.")
         
-        dirs = self.__getdirs()
-        for dir in dirs:
+
+        for dir in DirMgr(self.logDir):
             self.__processdir(dir)
         self.log.info("__process: Processed %d directories." % len(dirs))
             
         self.log.debug("__process: Leaving.")
 
-    def __getdirs(self):
-        '''
-        get the list of subdirectories underneath 'baseLogDir'
-        Each dir looks like <logDir>/2011-08-12/
-        '''
-
-        self.log.debug("__getdirs: Starting.")
-
-        if not os.access(self.logDir, os.F_OK):
-            self.log.warning('__getdirs: Base log directory %s does not exist - nothing to do',
-                      self.logDir)
-            self.log.warning("__getdirs: Leaving with no output.") 
-            return []
-        # else (==the base directory exists)
-        dirs = os.listdir(self.logDir)
-        # we only consider dirs looking like <logDir>/2011-08-12/
-        #   -- we need to insert <logDir> at the beginning
-        #   -- there is a file called robot.txt which does not match
-        logDirRe = re.compile(r"(\d{4})-(\d{2})-(\d{2})?$")
-        dirs = [os.path.join(self.logDir, d) for d in dirs if logDirRe.match(d)]
-
-        self.log.debug("__getdirs: Leaving with output %s." %entries) 
-        return entries
 
     def __processdir(self, dir):
         ''' 
         processes each directory.
-        Directories look like <logDir>/2011-08-12/ 
+        dir is a Dir object
         ''' 
 
         self.log.debug("__processdir: Starting with input %s." %dir)
-        self.__delsubdirs(dir)
-        self.__deldir(dir)
+
+        #dir.rm_subdirs(self.keepdays)
+
+        self.keepdays = KeepDays(self.fcl, self.qcl)
+        dir.rm(self.keepdays)
+
         self.log.debug("__processdir: Leaving.")
 
-    def __delsubdirs(self, dir):
-        '''
-        tries to delete all subdirectories
-        dir looks like <logDir>/2011-08-12
-        '''
-
-        self.log.debug('__delsubdirs: Starting for dir=%s.' %dir)
-
-        delta_t = self.__delta_t(dir)
-        subdirs = os.listdir(dir)
-        for subdir in subdirs:
-            self.__delsubdir(dir, subdir, delta_t)
-
-        self.log.debug('__delsubdir: Leaving.')
-
-    def __delsubdir(self, dir, subdir, delta_t):
-        '''
-        tries to delete each subdirectory
-        dir looks like <logDir>/2011-08-12
-        '''
-
-        self.log.debug('__delsubdir: Starting with inputs dir=%s, subdir=%s, delta_t=%s' %(dir, subdir, delta_t))
-
-        keep_days = self.queues_keepdays.get(subdir, self.factory_keepdays) 
-        if delta_t.days > keep_days:
-            path = os.join(dir, subdir)
-            self.log.info("__delsubdir: Entry %s is %d days old" % (path, delta_t.days))
-            if os.path.exists(path):
-                self.log.info("__delsubdir: Deleting %s ..." % path)
-                shutil.rmtree(path)
-
-        self.log.debug('__delsubdir: Leaving.')
-
-    def __delta_t(self, dir):
-        '''
-        returns how long since the directory was created.
-        It gets the creation time from the path itself.
-        Each directory looks like <logDir>/2011-08-12/
-        logDirMatch search for patterns like 2011-08-12
-        '''
-
-        self.log.debug("__delta_t: Starting for dir %s" %dir)
-
-        logDirRe = re.compile(r".*/(\d{4})-(\d{2})-(\d{2})?$")
-        logDirMatch = logDirRe.match(dir)
-        creation_t = datetime.date(int(logDirMatch.group(1)), 
-                                   int(logDirMatch.group(2)), 
-                                   int(logDirMatch.group(3)))
-        current_t = datetime.date.today()
-        delta_t = current_t - creation_t 
-        self.log.debug("__delta_t: Leaving with delta_t = %s" %delta_t)
-        return delta_t
-
-    def __deldir(self, dir):
-        '''
-        try to remove the directory dir
-        dir should look like  <logDir>/2011-08-12/
-        '''
-
-        self.log.debug('__deldir: Starting with dir=%s.' %dir)
-        if os.listdir(dir) == []:
-            # the dir is empty 
-            self.log.info("__deldir: dir %s is empty. We can delete it" % dir)
-            os.rmdir(dir)     
-        self.log.debug('__deldir: Leaving.')
-
-    def __getkeepdays(self):
-        '''
-        determines how old (in term of nb of days) 
-        can logs be w/o being removed
-        '''
-
-        self.log.debug("__getkeepdays: Starting.")
-
-        self.factory_keepdays = self.fcl.generic_get('factory', 'cleanlogs.keepdays', 'getint')
-        self.queues_keepdays = {}
-        for apfqname in self.qcl.sections():
-            keepdays = self.qcl.generic_get(apfqname, 'cleanlogs.keepdays', 'getint')
-            self.queues_keepdays[apfqname] = keepdays
-
-        self.log.debug("__getkeepdays: Leaving ")
 
     def __sleep(self):
         '''
@@ -210,6 +109,23 @@ class CleanLogs(threading.Thread):
 
 # =============================================================
 
+class KeepDays(object):
+
+    def __init__(self, fcl, qcl):
+        self.fcl = fcl
+        self.qcl = qcl
+        self.__inspect()
+
+    def __inspect(self):
+        self.factory_keepdays = self.fcl.generic_get('factory', 'cleanlogs.keepdays', 'getint')
+        self.queues_keepdays = {}
+        for apfqname in self.qcl.sections():
+            keepdays = self.qcl.generic_get(apfqname, 'cleanlogs.keepdays', 'getint')
+            self.queues_keepdays[apfqname] = keepdays
+
+    def get(self, apfqname):
+        return self.queues_keepdays.get(apfqname, self.factory_keepdays) 
+  
 
 class DirMgr(object):
     '''
@@ -228,7 +144,7 @@ class DirMgr(object):
             return []
         # else (==the base directory exists)
         dirs = []
-        for d in os.listdir(self.basedir)
+        for d in os.listdir(self.basedir):
             dir_obj = Dir(self.basedir, d)
             if dir_obj:
                 dirs.append(dir_obj)
@@ -242,7 +158,7 @@ class Dir(object):
     The parent directory looks like <logDir>/2011-08-12/ 
     '''
 
-    def __new__(self, basedir, dir):
+    def __new__(cls, basedir, dir):
         dirRe = re.compile(r"(\d{4})-(\d{2})-(\d{2})?$")
         if dirRe.match(dir):
             return super(Dir, cls).__new__(cls) 
@@ -282,15 +198,26 @@ class Dir(object):
         ''' 
         returns the list of subdirs 
         ''' 
-        #return os.listdir(path)
         subdirs = []
         for subdir in os.listdir(self.path):
-            subdirs.append(SubDir(self, subdir)
-        return subdirs
+            subdirs.append(SubDir(self, subdir))
+        return subdirs 
 
-    def del(self):
+    def rm(self, keepdays):
+
+        self.rm_subdirs(keepdays)
+
         if self.empty(): 
             os.rmdir(self.path)     
+
+    def rm_subdirs(self, keepdays):
+        '''
+        tries to remove as many subdirs as possible
+        keepdays is a KeepDays object
+        '''
+        
+        for subdir in self.subdirs():
+            subdir.rm(keepdays)
 
 
 class SubDir(object):
@@ -299,9 +226,18 @@ class SubDir(object):
     Subdirs look like <logDir>/2011-08-11/ANALY_BNL/
     '''
     def __init__(self, parent, subdir):
+        '''
+        parent is a Dir object
+        subdir is the APFQname
+        '''
         self.parent = parent
         self.subdir = subdir
         self.path = os.path.join(parent.path, subdir)
 
-    def del(self):
-        os.rmtree(self.path)
+    def rm(self, keepdays):
+
+        delta_t = self.parent.delta_t.days
+        if delta_t > keepdays.get(self.subdir):
+            if os.path.exists(self.path):
+                #self.log.info("__delsubdir: Deleting %s ..." % path)
+                shutil.rmtree(self.path)
