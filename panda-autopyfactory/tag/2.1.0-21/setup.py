e52c8a3ea048f136a28b5a67f8fcd33902dacaa7
jhover <jhover@d4488797-78bc-619e-cc48-e4164af92aee>
2012-05-21 20:38:57 +0000

    Tag for RPM rev 21.

---
+++ panda-autopyfactory/tag/2.1.0-21/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#
+# Setup prog for autopyfactory
+#
+#
+release_version='2.1.0'
+
+import re
+import sys
+import commands
+from distutils.core import setup
+from distutils.command.install import install as install_org
+from distutils.command.install_data import install_data as install_data_org
+
+# Python version check. 
+major, minor, release, st, num = sys.version_info
+if major == 2:
+    if not minor >= 4:
+        print("Autopyfactory requires Python >= 2.4. Exitting.")
+        sys.exit(0)
+
+rpm_data_files=[  ('/usr/libexec', ['libexec/runpilot3-wrapper.sh',
+                                  'libexec/wrapper.sh',                                 
+                                  ]),
+                  ('/etc/apf', ['etc/factory.conf-example',
+                              'etc/queues.conf-example',
+                              'etc/proxy.conf-example',
+                              'etc/factory.sysconfig-example',
+                             ]),
+                  ('/etc/init.d', ['etc/factory',
+                                ]),
+                  ('/etc/logrotate.d', ['etc/factory.logrotate',
+                                ]),                                        
+                ]
+
+home_data_files=[('libexec', ['libexec/runpilot3-wrapper.sh',
+                                  'libexec/wrapper.sh',                                 
+                                 ]),
+                ('etc', [ 'etc/factory.conf-example',
+                          'etc/queues.conf-example',
+                          'etc/proxy.conf-example',
+                          'etc/factory.sysconfig-example'
+                             ]),
+                ('etc', ['etc/factory',
+                                ]),      
+                ]
+
+def choose_data_files():
+    #print(sys.argv)
+    rpminstall = True
+    userinstall = False
+     
+    if 'bdist_rpm' in sys.argv:
+        rpminstall = True
+
+    elif 'install' in sys.argv:
+        for a in sys.argv:
+            if a.lower().startswith('--home'):
+                rpminstall = False
+                userinstall = True
+                
+    if rpminstall:
+        return rpm_data_files
+    elif userinstall:
+        return home_data_files
+    else:
+        # Something probably went wrong, so punt
+        return rpm_data_files
+       
+# setup for distutils
+setup(
+    name="panda-autopyfactory",
+    version=release_version,
+    description='panda-autopyfactory package',
+    long_description='''This package contains autopyfactory''',
+    license='GPL',
+    author='Panda Team',
+    author_email='hn-atlas-panda-pathena@cern.ch',
+    maintainer='Jose Caballero',
+    maintainer_email='jcaballero@bnl.gov',
+    url='https://twiki.cern.ch/twiki/bin/view/Atlas/PanDA',
+    packages=['autopyfactory','autopyfactory.plugins'],
+    scripts = [ # Utilities and main script
+               'bin/factory',
+               'misc/apfqueue-status',
+              ],
+    
+    data_files = choose_data_files()
+)
