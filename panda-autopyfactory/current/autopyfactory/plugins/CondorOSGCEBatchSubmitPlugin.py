0f71646721a1a3d23892c40f4014970736e20d5f
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-06-22 14:32:16 +0000

    adding TransferOutput tweak in OSGCE plugin

--- panda-autopyfactory/current/autopyfactory/plugins/CondorOSGCEBatchSubmitPlugin.py
+++ panda-autopyfactory/current/autopyfactory/plugins/CondorOSGCEBatchSubmitPlugin.py
@@ -55,6 +55,7 @@ class CondorOSGCEBatchSubmitPlugin(CondorCEBatchSubmitPlugin):
 
         self.JSD.add('grid_resource=condor %s' % self.gridresource) 
         #self.JSD.add('remote_universe = Local')
+        self.JSD.add('+TransferOutput=""')
         super(CondorOSGCEBatchSubmitPlugin, self)._addJSD()
 
         self.log.debug('CondorOSGCEBatchSubmitPlugin.addJSD: Leaving.')
