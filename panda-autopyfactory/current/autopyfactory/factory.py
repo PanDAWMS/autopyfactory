e1e9a5fdc5a4947f0258f6dce0c86aca475016b3
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2012-11-20 19:20:42 +0000

    bug fixed

--- panda-autopyfactory/current/autopyfactory/factory.py
+++ panda-autopyfactory/current/autopyfactory/factory.py
@@ -1182,7 +1182,7 @@ class PluginDispatcher(object):
             self.log.debug("_getplugin: Attempting to return plugin with classname %s" %plugin_class)
             self.log.debug("_getplugin: Leaving with plugin named %s" %plugin_class)
 
-            plugin_class = getattr(plugin_module, plugin_class)  # with getattr() we extract the actual class from the module object
+            plugin_class = getattr(plugin_module, plugin_class_name)  # with getattr() we extract the actual class from the module object
 
             ph.plugin_class_name = plugin_class_name 
             ph.plugin_module_name = plugin_module_name 
