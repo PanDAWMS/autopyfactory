8c394be9fce9d1f1b30c3c65851bef365eba6223
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2013-04-22 20:49:30 +0000

    bug fixed

--- panda-autopyfactory/current/autopyfactory/configloader.py
+++ panda-autopyfactory/current/autopyfactory/configloader.py
@@ -323,7 +323,7 @@ class ConfigManager(object):
             f = open(path)
             return f
         except:
-            raise FactoryConfigurationFailure("Problem with config file %s" % path)
+            raise ConfigFailure("Problem with config file %s" % path)
     
     
     def __dataFromURI(self, uri):
@@ -336,7 +336,7 @@ class ConfigManager(object):
             uridata = urllib2.urlopen(uri)
             return uridata
         except:
-            raise FactoryConfigurationFailure("Problem with URI source %s" % uri)
+            raise ConfigFailure("Problem with URI source %s" % uri)
 
 
 
