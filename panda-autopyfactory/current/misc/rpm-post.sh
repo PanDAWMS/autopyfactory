8b7de63c29dbae3e6a9693f5a29ce4166941bd57
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2013-07-01 19:55:44 +0000

    adding code to install the man pages

--- panda-autopyfactory/current/misc/rpm-post.sh
+++ panda-autopyfactory/current/misc/rpm-post.sh
@@ -13,3 +13,20 @@ SYSCONFEXAMPLE=/etc/apf/factory.sysconfig-example
 if [ ! -f $SYSCONF ] ; then 
         cp $SYSCONFEXAMPLE $SYSCONF
 fi
+
+
+# --- install the man pages, only if root  ---
+gzip /usr/share/doc/apf/autopyfactory.1
+mv /usr/share/doc/apf/autopyfactory.1.gz /usr/share/man/man1/
+
+gzip /usr/share/doc/apf/autopyfactory-queues.conf.5
+mv /usr/share/doc/apf/autopyfactory-queues.conf.5.gz /usr/share/man/man5/
+
+gzip /usr/share/doc/apf/autopyfactory-factory.conf.5
+mv /usr/share/doc/apf/autopyfactory-factory.conf.5.gz /usr/share/man/man5/ 
+
+gzip /usr/share/doc/apf/autopyfactory-proxy.conf.5
+mv /usr/share/doc/apf/autopyfactory-proxy.conf.5.gz /usr/share/man/man5/ 
+
+gzip /usr/share/doc/apf/autopyfactory-monitor.conf.5
+mv /usr/share/doc/apf/autopyfactory-monitor.conf.5.gz /usr/share/man/man5/  
