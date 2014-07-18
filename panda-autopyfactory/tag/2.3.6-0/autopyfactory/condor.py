29bf0bee40a0943a2582473b8497be1df25b56f2
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2014-07-18 16:06:26 +0000

    checking if out is XML even when RC!=0

--- panda-autopyfactory/tag/2.3.6-0/autopyfactory/condor.py
+++ panda-autopyfactory/tag/2.3.6-0/autopyfactory/condor.py
@@ -216,8 +216,12 @@ def querycondor(queryargs=None):
     if p.returncode == 0:
         log.debug('Leaving with OK return code.') 
     else:
-        log.warning('Leaving with bad return code. rc=%s err=%s' %(p.returncode, err ))
-        out = None
+        # lets try again. Sometimes RC!=0 does not mean the output was bad
+        if out.startswith('<?xml version="1.0"?>'):
+            log.warning('RC was %s but output is still valid' %p.returncode)
+        else:
+            log.warning('Leaving with bad return code. rc=%s err=%s' %(p.returncode, err ))
+            out = None
     log.trace('_querycondor: Out is %s' % out)
     log.debug('_querycondor: Leaving.')
     return out
