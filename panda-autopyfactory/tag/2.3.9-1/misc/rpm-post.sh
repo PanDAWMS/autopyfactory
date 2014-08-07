557657fb3ca2599a771fd0484333ff3a93013747
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2014-08-07 18:38:23 +0000

    tag 2.3.9

---
+++ panda-autopyfactory/tag/2.3.9-1/misc/rpm-post.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#if [ -f /etc/apf/factory.conf.bak ] ; then
+#	cp -f /etc/apf/factory.conf /etc/apf/factory.conf.rpmnew
+#	cp -f /etc/apf/factory.conf.bak /etc/apf/factory.conf
+#fi
+chmod ugo+x /etc/init.d/factory
+#chmod ugo+x /usr/libexec/wrapper.sh
+/sbin/chkconfig --add factory
+
+# By default on install set factory off?
+#/sbin/chkconfig factory off
+
+#  check that factory.sysconfig has been placed in /etc/sysconfig/factory.sysconfig 
+SYSCONF=/etc/sysconfig/factory.sysconfig
+SYSCONFEXAMPLE=/etc/apf/factory.sysconfig-example
+if [ ! -f $SYSCONF ] ; then 
+        cp $SYSCONFEXAMPLE $SYSCONF
+fi
+
+
+# --- install the man pages, only if root  ---
+gzip /usr/share/doc/apf/autopyfactory.1
+mv -f /usr/share/doc/apf/autopyfactory.1.gz /usr/share/man/man1/
+
+gzip /usr/share/doc/apf/autopyfactory-queues.conf.5
+mv -f /usr/share/doc/apf/autopyfactory-queues.conf.5.gz /usr/share/man/man5/
+
+gzip /usr/share/doc/apf/autopyfactory-factory.conf.5
+mv -f /usr/share/doc/apf/autopyfactory-factory.conf.5.gz /usr/share/man/man5/ 
+
+gzip /usr/share/doc/apf/autopyfactory-proxy.conf.5
+mv -f /usr/share/doc/apf/autopyfactory-proxy.conf.5.gz /usr/share/man/man5/ 
+
+gzip /usr/share/doc/apf/autopyfactory-monitor.conf.5
+mv -f /usr/share/doc/apf/autopyfactory-monitor.conf.5.gz /usr/share/man/man5/  
