e9a22fd97207380672449e9649299cc6950c2407
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2014-09-08 18:31:41 +0000

    new nomenclature

--- panda-autopyfactory/trunk/misc/rpm-post.sh
+++ panda-autopyfactory/trunk/misc/rpm-post.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-#if [ -f /etc/apf/factory.conf.bak ] ; then
-#	cp -f /etc/apf/factory.conf /etc/apf/factory.conf.rpmnew
-#	cp -f /etc/apf/factory.conf.bak /etc/apf/factory.conf
+#if [ -f /etc/autopyfactory/autopyfactory.conf.bak ] ; then
+#	cp -f /etc/autopyfactory/autopyfactory.conf /etc/autopyfactory/autopyfactory.conf.rpmnew
+#	cp -f /etc/autopyfactory/autopyfactory.conf.bak /etc/autopyfactory/autopyfactory.conf
 #fi
-chmod ugo+x /etc/init.d/factory
+chmod ugo+x /etc/init.d/autopyfactory
 #chmod ugo+x /usr/libexec/wrapper.sh
-/sbin/chkconfig --add factory
+/sbin/chkconfig --add autopyfactory
 
 # By default on install set factory off?
-#/sbin/chkconfig factory off
+#/sbin/chkconfig autopyfactory off
 
 #  check that factory.sysconfig has been placed in /etc/sysconfig/factory.sysconfig 
-SYSCONF=/etc/sysconfig/factory.sysconfig
-SYSCONFEXAMPLE=/etc/apf/factory.sysconfig-example
+SYSCONF=/etc/sysconfig/autopyfactory.sysconfig
+SYSCONFEXAMPLE=/etc/autopyfactory/autopyfactory.sysconfig-example
 if [ ! -f $SYSCONF ] ; then 
         cp $SYSCONFEXAMPLE $SYSCONF
 fi
 
 
 # --- install the man pages, only if root  ---
-gzip /usr/share/doc/apf/autopyfactory.1
-mv -f /usr/share/doc/apf/autopyfactory.1.gz /usr/share/man/man1/
+gzip /usr/share/doc/autopyfactory/autopyfactory.1
+mv -f /usr/share/doc/autopyfactory/autopyfactory.1.gz /usr/share/man/man1/
 
-gzip /usr/share/doc/apf/autopyfactory-queues.conf.5
-mv -f /usr/share/doc/apf/autopyfactory-queues.conf.5.gz /usr/share/man/man5/
+gzip /usr/share/doc/autopyfactory/autopyfactory-queues.conf.5
+mv -f /usr/share/doc/autopyfactory/autopyfactory-queues.conf.5.gz /usr/share/man/man5/
 
-gzip /usr/share/doc/apf/autopyfactory-factory.conf.5
-mv -f /usr/share/doc/apf/autopyfactory-factory.conf.5.gz /usr/share/man/man5/ 
+gzip /usr/share/doc/autopyfactory/autopyfactory-factory.conf.5
+mv -f /usr/share/doc/autopyfactory/autopyfactory-factory.conf.5.gz /usr/share/man/man5/ 
 
-gzip /usr/share/doc/apf/autopyfactory-proxy.conf.5
-mv -f /usr/share/doc/apf/autopyfactory-proxy.conf.5.gz /usr/share/man/man5/ 
+gzip /usr/share/doc/autopyfactory/autopyfactory-proxy.conf.5
+mv -f /usr/share/doc/autopyfactory/autopyfactory-proxy.conf.5.gz /usr/share/man/man5/ 
 
-gzip /usr/share/doc/apf/autopyfactory-monitor.conf.5
-mv -f /usr/share/doc/apf/autopyfactory-monitor.conf.5.gz /usr/share/man/man5/  
+gzip /usr/share/doc/autopyfactory/autopyfactory-monitor.conf.5
+mv -f /usr/share/doc/autopyfactory/autopyfactory-monitor.conf.5.gz /usr/share/man/man5/  
--- panda-autopyfactory/trunk/misc/rpm-pre.sh
+++ panda-autopyfactory/trunk/misc/rpm-pre.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#if [ -f /etc/apf/factory.conf ] ; then
-#	cp -f /etc/apf/factory.conf /etc/apf/factory.conf.bak
+#if [ -f /etc/autopyfactory/autopyfactory.conf ] ; then
+#	cp -f /etc/autopyfactory/autopyfactory.conf /etc/autopyfactory/autopyfactory.conf.bak
 #fi
 
-if id apf > /dev/null 2>&1; then
+if id autopyfactory > /dev/null 2>&1; then
 	: # do nothing
 else
-    /usr/sbin/useradd --comment "AutoPyFactory service account" --shell /bin/bash apf
+    /usr/sbin/useradd --comment "AutoPyFactory service account" --shell /bin/bash autopyfactory
 fi 
