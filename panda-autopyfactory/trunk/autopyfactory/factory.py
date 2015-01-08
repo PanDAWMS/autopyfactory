ffab29c5c421f8fc05ce1eed722338808e11ad67
jhover <jhover@d4488797-78bc-619e-cc48-e4164af92aee>
2015-01-08 13:54:36 +0000

    Increment version.

--- panda-autopyfactory/trunk/autopyfactory/factory.py
+++ panda-autopyfactory/trunk/autopyfactory/factory.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 
 __author__ = "Graeme Andrew Stewart, John Hover, Jose Caballero"
-__copyright__ = "2007,2008,2009,2010 Graeme Andrew Stewart; 2010-2014 John Hover; 2010-2014 Jose Caballero"
+__copyright__ = "2007,2008,2009,2010 Graeme Andrew Stewart; 2010-2015 John Hover; 2010-2015 Jose Caballero"
 __credits__ = []
 __license__ = "GPL"
-__version__ = "2.4.1"
+__version__ = "2.4.2"
 __maintainer__ = "Jose Caballero"
 __email__ = "jcaballero@bnl.gov,jhover@bnl.gov"
 __status__ = "Production"
