20e5f0d20c7bde6f20317d3204ef94deae88fc59
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2015-08-07 14:58:15 +0000

    tag 2.4.3

---
+++ panda-autopyfactory/tag/2.4.3-1/autopyfactory/test.py
@@ -0,0 +1,41 @@
+#
+#
+#
+# 
+
+
+import logging
+import logging.handlers
+import sys
+import time
+
+
+def setuplogging():
+
+        log = logging.getLogger()
+        logStream = logging.StreamHandler(sys.stdout)
+
+        major, minor, release, st, num = sys.version_info
+        if major == 2 and minor == 4:
+            FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d : %(message)s'
+        else:
+            FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
+
+        formatter = logging.Formatter(FORMAT)
+        formatter.converter = time.gmtime  # to convert timestamps to UTC
+        logStream.setFormatter(formatter)
+        log.addHandler(logStream)
+        log.setLevel(logging.DEBUG)
+
+
+class MockAPFQueue(object):
+    '''
+     Used to build CondorEC2BatchStatusPlugin object for testing...
+    '''
+
+    def __init__(self, apfqname):
+        self.apfqname = apfqname
+
+
+
+
