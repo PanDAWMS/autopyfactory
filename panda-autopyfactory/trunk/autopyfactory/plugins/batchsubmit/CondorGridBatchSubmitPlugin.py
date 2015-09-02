e2cb9430fac4cc800b500be963f89a458ff4f194
jcaballe <jcaballe@d4488797-78bc-619e-cc48-e4164af92aee>
2015-09-02 17:49:46 +0000

    moved x509 proxy management from CondorBase submit plugin to CondorGrid submit plugin

--- panda-autopyfactory/trunk/autopyfactory/plugins/batchsubmit/CondorGridBatchSubmitPlugin.py
+++ panda-autopyfactory/trunk/autopyfactory/plugins/batchsubmit/CondorGridBatchSubmitPlugin.py
@@ -4,6 +4,7 @@
 #
 
 from CondorBaseBatchSubmitPlugin import CondorBaseBatchSubmitPlugin 
+from autopyfactory.apfexceptions import InvalidProxyFailure
 from autopyfactory import jsd 
 
 
@@ -14,11 +15,42 @@ class CondorGridBatchSubmitPlugin(CondorBaseBatchSubmitPlugin):
             qcl = apfqueue.qcl            
         else:
             qcl = config
-       
-               
         newqcl = qcl.clone().filterkeys('batchsubmit.condorgrid', 'batchsubmit.condorbase')
         super(CondorGridBatchSubmitPlugin, self).__init__(apfqueue, newqcl) 
+        
+        # ---- proxy management ------
+        self.x509userproxy = None
+        self.proxylist = None
+        try:
+            plist = qcl.generic_get(self.apfqname, 'batchsubmit.condorgrid.proxy')
+            # This is alist of proxy profile names specified in proxy.conf
+            # We will only attempt to derive proxy file path during submission
+            if plist:
+                self.proxylist = [x.strip() for x in plist.split(',')]
+            self._getX509Proxy()
+        except InvalidProxyFailure, ipf:
+            self.log.error('Unable to get valid proxy file.')
+            raise
+        except Exception, e:
+            self.log.error("Caught exception: %s " % str(e))
+            raise
+        except:
+            raise
+
         self.log.info('CondorGridBatchSubmitPlugin: Object initialized.')
+
+
+    def _getX509Proxy(self):
+        '''
+        uses proxymanager to find out the path to the X509 file
+        '''
+    
+        self.log.debug("Determining proxy, if necessary. Profile: %s" % self.proxylist)
+        if self.proxylist:
+            self.x509userproxy = self.factory.proxymanager.getProxyPath(self.proxylist)
+        else:
+            self.log.debug("No proxy profile defined.")
+
 
     def _addJSD(self):
         '''   
@@ -28,6 +60,13 @@ class CondorGridBatchSubmitPlugin(CondorBaseBatchSubmitPlugin):
         self.log.debug('CondorGridBatchSubmitPlugin.addJSD: Starting.')
    
         self.JSD.add("universe", "grid")
+        # -- proxy path --
+        if self.x509userproxy:
+            self.JSD.add("x509userproxy", "%s" % self.x509userproxy)
+        else:
+            self.log.critical('no x509 proxy found. Aborting')
+            raise Exception 
+
         super(CondorGridBatchSubmitPlugin, self)._addJSD()
     
         self.log.debug('CondorGridBatchSubmitPlugin.addJSD: Leaving.')
