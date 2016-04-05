de9a35579ddff4780029d5e3ab49560f8b6e550b
John Hover <jhover@users.noreply.github.com>
2016-04-05 20:09:29 +0000

    Adding back x509 proxy handling to CondorLocal batchsubmit

--- autopyfactory/plugins/queue/batchsubmit/CondorLocal.py
+++ autopyfactory/plugins/queue/batchsubmit/CondorLocal.py
@@ -19,24 +19,42 @@ class CondorLocal(CondorBase):
             qcl = apfqueue.qcl            
         else:
             qcl = config
+            
+        plist = qcl.generic_get(self.apfqname, 'batchsubmit.condorbase.proxy')
+        # This is alist of proxy profile names specified in proxy.conf
+        # We will only attempt to derive proxy file path during submission
+        if plist:
+            self.proxylist = [x.strip() for x in plist.split(',')]
         newqcl = qcl.clone().filterkeys('batchsubmit.condorlocal', 'batchsubmit.condorbase')
 
         super(CondorLocal, self).__init__(apfqueue, config=newqcl) 
         self.log.info('CondorLocal: Object initialized.')
 
+    def _getX509Proxy(self):
+        '''
+        
+        '''
+        self.log.debug("Determining proxy, if necessary. Profile: %s" % self.proxylist)
+        if self.proxylist:
+            self.x509userproxy = self.factory.proxymanager.getProxyPath(self.proxylist)
+        else:
+            self.log.debug("No proxy profile defined.")       
        
     def _addJSD(self):
         '''
         add things to the JSD object
         '''
 
-        self.log.debug('CondorLocal.addJSD: Starting.')
-
+        self.log.trace('CondorLocal.addJSD: Starting.')
+        
+        # -- proxy path --
+        if self.x509userproxy:
+            self.JSD.add("x509userproxy", "%s" % self.x509userproxy)
         self.JSD.add("universe", "vanilla")
         self.JSD.add("should_transfer_files", "IF_NEEDED")
         self.JSD.add('+TransferOutput', '""')
 
         super(CondorLocal, self)._addJSD()
 
-        self.log.debug('CondorLocal.addJSD: Leaving.')
+        self.log.trace('CondorLocal.addJSD: Leaving.')
     
