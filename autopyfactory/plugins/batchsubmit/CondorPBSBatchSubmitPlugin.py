91004f36589f70ca1ec473e3ccb683867b530e02
John Hover <jhover@users.noreply.github.com>
2016-05-04 19:39:21 +0000

    Switching to standard dir name.

---
+++ autopyfactory/plugins/batchsubmit/CondorPBSBatchSubmitPlugin.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+#
+# AutoPyfactory batch plugin for Condor
+#
+
+from CondorGridBatchSubmitPlugin import CondorGridBatchSubmitPlugin
+from autopyfactory import jsd
+
+
+class CondorPBSBatchSubmitPlugin(CondorGridBatchSubmitPlugin):
+    id = 'condorpbs'
+    
+    def __init__(self, apfqueue, config=None):
+        if not config:
+            qcl = apfqueue.factory.qcl            
+        else:
+            qcl = config
+        newqcl = qcl.clone().filterkeys('batchsubmit.condorpbs', 'batchsubmit.condorgrid')
+        super(CondorPBSBatchSubmitPlugin, self).__init__(apfqueue, config=newqcl)        
+
+        self.log.info('CondorPBSBatchSubmitPlugin: Object initialized.')
+
+    def _addJSD(self):
+        '''
+        add things to the JSD object
+        '''
+
+        self.log.debug('CondorPBSBatchSubmitPlugin.addJSD: Starting.')
+
+        self.JSD.add('grid_resource', 'pbs') 
+        super(CondorPBSBatchSubmitPlugin, self)._addJSD()
+
+        self.log.debug('CondorPBSBatchSubmitPlugin.addJSD: Leaving.')
+
+
