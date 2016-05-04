ed7766dc80b622d19e40b1d9b4cf3ffab4a37216
John Hover <jhover@users.noreply.github.com>
2016-05-04 19:20:07 +0000

    Switching to standard name.

---
+++ autopyfactory/plugins/sched/ThrottleSchedPlugin.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+
+import datetime
+import logging
+import htcondor
+
+from autopyfactory.interfaces import SchedInterface
+
+
+class ThrottleSchedPlugin(SchedInterface):
+    id = 'throttle'
+    
+    def __init__(self, apfqueue):
+
+        try:
+            self.apfqueue = apfqueue                
+            self.log = logging.getLogger("main.schedplugin[%s]" % apfqueue.apfqname)
+            try:
+                self.offset = self.apfqueue.qcl.generic_get(self.apfqueue.apfqname, 'sched.ready.offset', 'getint', default_value=0)
+                self.log.debug("SchedPlugin: offset = %d" % self.offset)
+            except:
+                pass 
+                # Not mandatory
+                
+            self.log.debug("SchedPlugin: Object initialized.")
+        except Exception, ex:
+            self.log.error("SchedPlugin object initialization failed. Raising exception")
+            raise ex
+
+
+    def calcSubmitNum(self, n=0):
+        """ 
+        """
+
+        out = n
+        self.log.debug('Starting.')
+        (out, msg) = self._calc(n)
+        return (out, msg)
+
+
+    def _calc(self, input):
+        '''
+        algorithm 
+        '''
+        
+
+
+
+        # configuration variables we need:
+        #   -- the interval window to look at: last 30 minutes?, last 60 minutes?, ...
+        #   -- the max Wall Time to call a WN a black hole: jobs run in less than 5 minutes?, than 30 minutes?, ...
+        #   -- the minimum number of black hole type jobs to start paying attention
+        #           -- by number or by fraction?
+        #   -- how much to throttle is a black hole is detected.
+
+
+
+        # to convert the current date into seconds since Epoch
+        now_sec_epoch = datetime.datetime.now().strftime('%s')
+
+        # condor_history using the python bindings
+        schedd = htcondor.Schedd()
+        out = schedd.history("RemoteWallClockTime < 600 && MATCH_APF_QUEUE == \"ANALY_BNL_LONG-gridgk03-htcondor\"", ['ClusterId, ProcID'], 0)
+        n_pilots = sum(1 for o in out)
+
+        # in the constraint expression we need to add the pilot was queued after a given time
+        # that given time is  
+        #           now_sec_epoch - interval
+        # and the Condor ClassAd to look at is QDate, or JobStartDate
+
+
+
+
+         
+
+        out = max(0, ( activated_jobs - self.offset)  - pending_pilots )
+        self.log.info('input=%s; activated=%s; offset=%s pending=%s; running=%s; Return=%s' %(input,
+                                                                                         activated_jobs,
+                                                                                         self.offset, 
+                                                                                         pending_pilots, 
+                                                                                         running_pilots, 
+                                                                                         out))
+        msg = "Ready:in=%s;activated=%d,offset=%d,pending=%d;ret=%d" % (input, activated_jobs, self.offset, pending_pilots, out)
+        return (out,msg)
--- setup.py
+++ setup.py
@@ -30,19 +30,19 @@ if major == 2:
 
 libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
 
-etc_files = ['etc/autopyfactory.conf',
-             'etc/queues.conf',
-             'etc/proxy.conf',
-             'etc/monitor.conf',
-             'etc/mappings.conf',
+etc_files = ['etc/autopyfactory.conf-example',
+             'etc/queues.conf-example',
+             'etc/proxy.conf-example',
+             'etc/monitor.conf-example',
+             'etc/mappings.conf-example',
              ]
 
 sysconfig_files = [
-             'etc/sysconfig/autopyfactory',
-             'etc/sysconfig/proxymanager',
+             'etc/sysconfig/autopyfactory-example',
+             'etc/sysconfig/proxymanager-example',
 ]
 
-logrotate_files = ['etc/logrotate/autopyfactory',]
+logrotate_files = ['etc/logrotate/autopyfactory-example',]
 
 initd_files = ['etc/autopyfactory',
                'etc/proxymanager']
@@ -50,15 +50,23 @@ initd_files = ['etc/autopyfactory',
 # NOTES: the docs are actually handled by setup.cfg. They are moved directory under /usr/share/doc/autopyfactory-<version>/
 docs_files = ['docs/%s' %file for file in os.listdir('docs') if os.path.isfile('docs/%s' %file)]
 
+# at least for the time being, we are not going to distribute manpages, given disutils does not support them properly
+#man_files = ['docs/man/%s' %file for file in os.listdir('docs/man') if os.path.isfile('docs/man/%s' %file)]
+#man1_files = ['docs/man/%s' %file for file in os.listdir('docs/man') if os.path.isfile('docs/man/%s' %file) and file.endswith('.1')]
+#man5_files = ['docs/man/%s' %file for file in os.listdir('docs/man') if os.path.isfile('docs/man/%s' %file) and file.endswith('.5')]
+
 
 # -----------------------------------------------------------
 
 rpm_data_files=[#('/etc/autopyfactory', libexec_files),
-                ('/etc/autopyfactory', etc_files),
-                ('/etc/sysconfig', sysconfig_files),
-                ('/etc/logrotate.d', logrotate_files),                                        
+                #('/etc/autopyfactory', etc_files),
+                #('/etc/sysconfig', sysconfig_files),
+                #('/etc/logrotate.d', logrotate_files),                                        
                 ('/etc/init.d', initd_files),
                 #('/usr/share/doc/autopyfactory', docs_files),                                        
+                #('/tmp', man_files),                                        
+                #('/usr/share/man/man1', man1_files),
+                #('/usr/share/man/man5', man5_files),
                ]
 
 
@@ -108,20 +116,14 @@ setup(
     url='https://twiki.cern.ch/twiki/bin/view/Atlas/PanDA',
     packages=['autopyfactory',
               'autopyfactory.plugins',
-              'autopyfactory.plugins.factory',
-              'autopyfactory.plugins.factory.config',
-              'autopyfactory.plugins.queue',
-              'autopyfactory.plugins.queue.batchstatus',
-              'autopyfactory.plugins.queue.batchsubmit',
-              'autopyfactory.plugins.queue.monitor',
-              'autopyfactory.plugins.queue.sched',
-              'autopyfactory.plugins.queue.wmsstatus',
-              'autopyfactory.external',
-              'autopyfactory.external.panda',
+              'autopyfactory.plugins.batchstatus',
+              'autopyfactory.plugins.batchsubmit',
+              'autopyfactory.plugins.monitor',
+              'autopyfactory.plugins.sched',
+              'autopyfactory.plugins.wmsstatus',
               ],
     scripts = [ # Utilities and main script
                'bin/autopyfactory',
-               'bin/autopyfactory_version',
                'bin/proxymanager'
               ],
     
