38a0f060fdd11abe5389f7dcbd699ef864cf3ef1
John Hover <jhover@users.noreply.github.com>
2016-05-04 19:39:21 +0000

    Switching to standard dir name.

---
+++ autopyfactory/plugins/sched/KeepNRunningSchedPlugin.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+#
+import logging
+
+from autopyfactory.interfaces import SchedInterface
+
+
+class KeepNRunningSchedPlugin(SchedInterface):
+    '''
+    This plugin strives to keep a certain number of jobs/pilots/VMs running, regardless 
+    of ready/activated or input.
+    
+    If config keep_running is None, then it changes the sense of input from 
+    new jobs (relative) to a target number (absolute) 
+    
+    Understands Retiring VM job state. 
+    
+    May output a negative number, if keep_running is less than current running. 
+      
+    '''
+    id = 'keepnrunning'
+    
+    def __init__(self, apfqueue):
+
+        try:
+            self.apfqueue = apfqueue                
+            self.log = logging.getLogger("main.schedplugin[%s]" % apfqueue.apfqname)
+            self.keep_running = self.apfqueue.qcl.generic_get(self.apfqueue.apfqname, 'sched.keepnrunning.keep_running', 'getint')
+            self.log.debug("SchedPlugin: Object initialized.")
+        except Exception, ex:
+            self.log.error("SchedPlugin object initialization failed. Raising exception")
+            raise ex
+
+    def calcSubmitNum(self, nsub=0):
+        """ 
+        It just returns nb of Activated Jobs - nb of Pending Pilots
+        """
+        self.log.debug('Starting.')
+
+        self.queueinfo = self.apfqueue.batchstatus_plugin.getInfo(queue = self.apfqueue.apfqname, maxtime = self.apfqueue.batchstatusmaxtime)
+       
+        if not self.queueinfo:
+            self.log.warning("self.queueinfo is None!")
+            out = 0
+            msg = "Invalid queueinfo"
+        else:
+            (out, msg) = self._calc(nsub)
+            self.log.debug("Returning %d" % out)
+        return (out, msg)
+
+    def _calc(self, input):
+        '''
+        algorithm 
+        '''
+        
+        # initial default values. 
+        pending_pilots = 0
+        running_pilots = 0
+        retiring_pilots = 0
+
+        pending_pilots = self.queueinfo.pending  # using the new info objects
+        running_pilots = self.queueinfo.running  # using the new info object
+        retiring_pilots = self.queueinfo.retiring # using the new info objects
+
+        # 
+        # Output is simply keep_running, minus potentially or currently running, while ignoring retiring jobs
+        #
+        if self.keep_running:
+            self.log.debug("keep_running is set %d, use it." % self.keep_running) 
+            out = self.keep_running - ( running_pilots  + pending_pilots)
+        else:
+            self.log.debug("keep_running is set not set, use input." % self.keep_running)
+            out = input - ( running_pilots + pending_pilots )
+
+        self.log.info('input=%s (ignored); keep_running=%s; pending=%s; running=%s; retiring=%s; Return=%s' %(input,
+                                                                                         self.keep_running, 
+                                                                                         pending_pilots, 
+                                                                                         running_pilots,
+                                                                                         retiring_pilots, 
+                                                                                         out))
+        msg = "KeepNRunning:in=%s,keep=%s,run=%s,pend=%s,retiring=%s,out=%s" % (str(input), 
+                                                                           self.keep_running, 
+                                                                           running_pilots, 
+                                                                           pending_pilots, 
+                                                                           retiring_pilots, 
+                                                                           out)
+        return (out, msg)
+
+
+
+
--- setup.py
+++ setup.py
@@ -30,43 +30,51 @@ if major == 2:
 
 libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
 
-etc_files = ['etc/autopyfactory.conf',
-             'etc/queues.conf',
-             'etc/proxy.conf',
-             'etc/monitor.conf',
-             'etc/mappings.conf',
+etc_files = ['etc/factory.conf-example',
+             'etc/queues.conf-example',
+             'etc/proxy.conf-example',
+             'etc/monitor.conf-example',
+             'etc/factory.sysconfig-example',
+             'etc/proxymanager.sysconfig-example',
+             'etc/logsmonitor.rotate.conf-example',
+             'etc/apf-search-failed.sh-example',
              ]
 
-sysconfig_files = [
-             'etc/sysconfig/autopyfactory',
-             'etc/sysconfig/proxymanager',
-]
-
-logrotate_files = ['etc/logrotate/autopyfactory',]
-
-initd_files = ['etc/autopyfactory',
+initd_files = ['etc/factory',
                'etc/proxymanager']
 
-# NOTES: the docs are actually handled by setup.cfg. They are moved directory under /usr/share/doc/autopyfactory-<version>/
-docs_files = ['docs/%s' %file for file in os.listdir('docs') if os.path.isfile('docs/%s' %file)]
+logrotate_files = ['etc/factory.logrotate',]
 
+# docs files:
+#   --everything in the docs/ directory
+#   -- RELEASE_NOTES file 
+docs_files = ['docs/%s' %file for file in os.listdir('docs') if os.path.isfile('docs/%s' %file)]
+docs_files.append('RELEASE_NOTES')
+
+utils_files = ['misc/apf-agis-config',
+               'misc/apf-queue-status',
+               'misc/apf-queue-jobs-by-status.sh',
+               'misc/apf-test-pandaclient',
+               'misc/apf-check-old-pilots',
+               'misc/apf-search-failed',
+               'misc/apf-simulate-scheds',
+               ]
 
 # -----------------------------------------------------------
 
-rpm_data_files=[#('/etc/autopyfactory', libexec_files),
-                ('/etc/autopyfactory', etc_files),
-                ('/etc/sysconfig', sysconfig_files),
-                ('/etc/logrotate.d', logrotate_files),                                        
-                ('/etc/init.d', initd_files),
-                #('/usr/share/doc/autopyfactory', docs_files),                                        
+rpm_data_files=[('/etc/apf',           libexec_files),
+                ('/etc/apf',           etc_files),
+                ('/etc/init.d',        initd_files),
+                ('/etc/logrotate.d',   logrotate_files),                                        
+                ('/usr/share/doc/apf', docs_files),                                        
+                ('/usr/share/apf',     utils_files),                                        
                ]
 
-
-home_data_files=[#('etc', libexec_files),
-                 ('etc', etc_files),
-                 ('etc', initd_files),
-                 ('etc', sysconfig_files),
-                 ('doc/autopyfactory', docs_files),
+home_data_files=[('etc',       libexec_files),
+                 ('etc',       etc_files),
+                 ('etc',       initd_files),
+                 ('doc/apf',   docs_files ),
+                 ('share/apf', utils_files),                                        
                 ]
 
 # -----------------------------------------------------------
@@ -96,9 +104,9 @@ def choose_data_files():
 
 # setup for distutils
 setup(
-    name="autopyfactory",
+    name="panda-autopyfactory",
     version=release_version,
-    description='autopyfactory package',
+    description='panda-autopyfactory package',
     long_description='''This package contains autopyfactory''',
     license='GPL',
     author='Panda Team',
@@ -108,20 +116,14 @@ setup(
     url='https://twiki.cern.ch/twiki/bin/view/Atlas/PanDA',
     packages=['autopyfactory',
               'autopyfactory.plugins',
-              'autopyfactory.plugins.factory',
-              'autopyfactory.plugins.factory.config',
-              'autopyfactory.plugins.queue',
-              'autopyfactory.plugins.queue.batchstatus',
-              'autopyfactory.plugins.queue.batchsubmit',
-              'autopyfactory.plugins.queue.monitor',
-              'autopyfactory.plugins.queue.sched',
-              'autopyfactory.plugins.queue.wmsstatus',
-              'autopyfactory.external',
-              'autopyfactory.external.panda',
+              'autopyfactory.plugins.batchstatus',
+              'autopyfactory.plugins.batchsubmit',
+              'autopyfactory.plugins.monitor',
+              'autopyfactory.plugins.sched',
+              'autopyfactory.plugins.wmsstatus',
               ],
     scripts = [ # Utilities and main script
-               'bin/autopyfactory',
-               'bin/autopyfactory_version',
+               'bin/factory',
                'bin/proxymanager'
               ],
     
