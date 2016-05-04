ff936599842048d184e72c555b85a1a1902d9ebe
John Hover <jhover@users.noreply.github.com>
2016-05-04 19:39:21 +0000

    Switching to standard dir name.

---
+++ autopyfactory/plugins/batchsubmit/CondorOSGCEBatchSubmitPlugin.py
@@ -0,0 +1,44 @@
+#!/bin/env python
+#
+# AutoPyfactory batch plugin for Condor
+#
+
+from CondorCEBatchSubmitPlugin import CondorCEBatchSubmitPlugin
+from autopyfactory import jsd
+
+
+class CondorOSGCEBatchSubmitPlugin(CondorCEBatchSubmitPlugin):
+    id = 'condorosgce'
+    '''
+    This class is expected to have separate instances for each PandaQueue object. 
+    '''
+   
+    def __init__(self, apfqueue, config=None):
+        if not config:
+            qcl = apfqueue.factory.qcl            
+        else:
+            qcl = config
+        newqcl = qcl.clone().filterkeys('batchsubmit.condorosgce', 'batchsubmit.condorce')
+        super(CondorOSGCEBatchSubmitPlugin, self).__init__(apfqueue, config=newqcl) 
+        try:
+            self.gridresource = qcl.generic_get(self.apfqname, 'batchsubmit.condorosgce.gridresource') 
+        except Exception, e:
+            self.log.error("Caught exception: %s " % str(e))
+            raise
+        
+        self.log.info('CondorOSGCEBatchSubmitPlugin: Object initialized.')
+
+    def _addJSD(self):
+        '''
+        add things to the JSD object
+        '''
+
+        self.log.debug('CondorOSGCEBatchSubmitPlugin.addJSD: Starting.')
+
+        self.JSD.add('grid_resource', 'condor %s' % self.gridresource) 
+        #self.JSD.add('remote_universe ', ' Local')
+        self.JSD.add('+TransferOutput', '""')
+        super(CondorOSGCEBatchSubmitPlugin, self)._addJSD()
+
+        self.log.debug('CondorOSGCEBatchSubmitPlugin.addJSD: Leaving.')
+
