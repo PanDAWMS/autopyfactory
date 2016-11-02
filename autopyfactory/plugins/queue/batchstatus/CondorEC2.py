8b770f3d6a69e36147c654c43e100c42fa3c459a
Jose Caballero <jcaballero@bnl.gov>
2016-11-02 10:27:02 -0400

    internal only methods in condor.py renamed with leading underscore

--- autopyfactory/plugins/queue/batchstatus/CondorEC2.py
+++ autopyfactory/plugins/queue/batchstatus/CondorEC2.py
@@ -23,7 +23,7 @@ from autopyfactory.info import BatchStatusInfo
 from autopyfactory.info import QueueInfo
 
 from autopyfactory.condor import checkCondor, querycondorxml, statuscondor, statuscondormaster
-from autopyfactory.condor import parseoutput, xml2nodelist, node2dict 
+from autopyfactory.condor import parseoutput
 from autopyfactory.condor import listnodesfromxml, aggregateinfo, killids
 from autopyfactory.condor import mincondorversion
 
