69dcd11dd119ef07a2651dfac4087dda629a41f5
Jose Caballero <jcaballero@bnl.gov>
2016-11-17 14:02:36 -0500

    aesthetic

--- autopyfactory/plugin.py
+++ autopyfactory/plugin.py
@@ -33,9 +33,8 @@
 Plugins initialized with ConfigParser object and a section name as arguments. The 
 assumption is that the classes themselves know how to look up the info they need. 
 It is also assumed that Singleton/non-Singleton decision is made during class initialization.
-
-
 '''
+
 import logging
 import logging.handlers
 import traceback
