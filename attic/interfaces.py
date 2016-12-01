e53e6f2a22dec735d0dc8d8bc5459f47de7d82c0
Jose Caballero <jcaballero@bnl.gov>
2016-12-01 14:04:18 -0500

    singletons removed from interfaces

--- attic/interfaces.py
+++ attic/interfaces.py
@@ -191,3 +191,109 @@ class MonitorInterface(object):
         '''
         raise NotImplementedError
    
+
+# ==============================================================================                                
+#                      SINGLETON CLASSES 
+# ==============================================================================  
+
+class Singleton(type):
+    '''
+    -----------------------------------------------------------------------
+    Ancillary class to be used as metaclass to make other classes Singleton.
+    -----------------------------------------------------------------------
+    '''
+    
+    def __init__(cls, name, bases, dct):
+        cls.__instance = None 
+        type.__init__(cls, name, bases, dct)
+    def __call__(cls, *args, **kw): 
+        if cls.__instance is None:
+            cls.__instance = type.__call__(cls, *args,**kw)
+        return cls.__instance
+
+
+class CondorSingleton(type):
+    '''
+    -----------------------------------------------------------------------
+    Ancillary class to be used as metaclass to make other classes Singleton.
+    This particular implementation is for CondorBatchStatusPlugin.
+    It allow to create different instances, one per schedd.
+    Each instance is a singleton. 
+    -----------------------------------------------------------------------
+    '''
+    
+    def __init__(cls, name, bases, dct):
+        cls.__instance = {} 
+        type.__init__(cls, name, bases, dct)
+
+    def __call__(cls, *args, **kw): 
+        condor_q_id = kw.get('condor_q_id', 'local')
+        if condor_q_id not in cls.__instance.keys():
+            cls.__instance[condor_q_id] = type.__call__(cls, *args,**kw)
+        return cls.__instance[condor_q_id]
+
+
+def singletonfactory(id_var=None, id_default=None):
+    '''
+    This is an abstraction of the two previous classes. 
+    We have here a metaclass factory, which will decide 
+    which type of Singleton metaclass returns based on the inputs
+
+    If id_var is not passed, then we asume a regular singleton __metaclass__ is expected.
+    If id_var has a value, then it is a multi-singleton.
+    We understand by multi-singleton a class that can instantiate the same object or not,
+    depending on the value of id_var. Same value of id_var will generate the same object.
+  
+    id_var is the name of a key variable to be passed via __init__() when asking for a new object.
+    The value of that variable will be the ID to determine if a real new object is needed or not.
+
+    Note: when calling __init__(), the id_var has to be passed as a key=value variable,
+    not just as a positional variable. 
+
+    Examples:
+
+        class A(object):
+            __metaclass__ = singletonfactory()
+
+        ---------------------------------------------------------------------
+
+        class B(object):
+            __metaclass__ = singletonfactory(id_var='condorpool', id_default='local')
+        
+        obj1 = B(..., condorpool='pool1', ...)
+        obj2 = B(..., condorpool='pool1', ...)
+        obj3 = B(..., condorpool='pool2', ...)
+
+        obj1 and obj2 will be the same. obj3 will not. 
+    '''
+
+    class Singleton(type):
+
+        # regular singleton __metaclass__
+        if not id_var:
+
+            def __init__(cls, name, bases, dct):
+                cls.__instance = None 
+                type.__init__(cls, name, bases, dct)
+            def __call__(cls, *args, **kw):
+                if cls.__instance is None:
+                    cls.__instance = type.__call__(cls, *args,**kw)
+                return cls.__instance
+
+        # multi-singleton __metaclass__
+        else:
+
+            def __init__(cls, name, bases, dct):
+                cls.__instance = {}
+                type.__init__(cls, name, bases, dct)
+            def __call__(cls, *args, **kw):
+                id = kw.get(id_var, id_default)
+                # note: we read the value of id_var from **kw
+                #       so it has to be passed as a key=value variable,
+                #       not as a positional variable. 
+                if id not in cls.__instance.keys():
+                    cls.__instance[id] = type.__call__(cls, *args,**kw)
+                return cls.__instance[id]
+
+    return Singleton
+
