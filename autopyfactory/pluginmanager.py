43d339c61b92eac533275a8d3e5c3fc70994fd49
Jose Caballero <jcaballero@bnl.gov>
2017-02-07 10:09:50 -0500

    new plugins management code

--- autopyfactory/pluginmanager.py
+++ autopyfactory/pluginmanager.py
@@ -41,69 +41,106 @@ import traceback
 
 from pprint import pprint
 
+class NullHandler(logging.Handler):
+    """
+    This handler does nothing. It's intended to be used to avoid the
+    "No handlers could be found for logger XXX" one-off warning. This is
+    important for library code, which may contain code to log events. If a user
+    of the library does not configure logging, the one-off warning might be
+    produced; to avoid this, the library developer simply needs to instantiate
+    a NullHandler and add it to the top-level logger of the library module or
+    package.
+    """
+    def handle(self, record):
+        pass
+
+    def emit(self, record):
+        pass
+
+    def createLock(self):
+        self.lock = None
+
+
+
 class PluginManager(object):
     '''
     Entry point for plugins creation, initialization, starting, and configuration. 
-    
     '''
+    
     def __init__(self):
         '''
         Top-level object to provide plugins. 
         '''
-        self.log = logging.getLogger('main.pluginmanager')
+        self.log = logging.getLogger('pluginsmanager')
+        self.log.addHandler(NullHandler())
         self.log.debug('PluginManager initialized.')
 
 
-    def getpluginlist(self, parent, level, kind, config, section, attribute):
+    def getpluginlist(self, parent, paths, namelist, config, section):
         '''
-        Provide list of initialized plugin objects. Convenience method mainly for sched 
-        plugins. 
+        Provides a list of initialized plugin objects. 
+        parent: reference to the calling object
+        paths: list of subdirectories from where to import the plugin(s)
+        namelist: list of plugins to be imported
+        config: config parser object to feed to the plugin
+        section: the section name in the config object that is relevant for this plugin
         '''
-        try:
-            namelist = config.get(section, attribute)
-        except:
-            return None
-        namelist = [i.strip() for i in namelist.split(',')]
         plist = []
         for name in namelist:
-            po = self._getplugin(parent, level, kind, config, section, name)
+            po = self._getplugin(parent, paths, name, config, section)
             plist.append(po)
+        self.log.info('returning a list of plugins = %s' %plist)
         return plist
 
 
-    def getplugin(self, parent, level, kind, config, section, attribute):
+    def getplugin(self, parent, paths, name, config, section):
         '''
-        Provide initialized plugin object using config and section. 
+        Provides a single initialized plugin object. 
+        parent: reference to the calling object
+        paths: list of subdirectories from where to import the plugin(s)
+        name: name of the single plugin to be imported
+        config: config parser object to feed to the plugin
+        section: the section name in the config object that is relevant for this plugin
         '''
-        try:
-            name = config.get(section, attribute)
-        except:
-            return None
-        return self._getplugin(parent, level, kind, config, section, name)
+        p = self._getplugin(parent, paths, name, config, section)
+        self.log.info('returning a plugin = %s' %p)
+        return p
 
 
-    def _getplugin(self, parent,  level, kind, config, section, name):
-        ko = self._getpluginclass(level, kind, name)
+    def _getplugin(self, parent, paths, name, config, section):
+        """
+        returns an initialized plugin object.
+        parent: reference to the calling object
+        paths: list of subdirectories from where to import the plugin(s)
+        name: name of the single plugin to be imported
+        config: config parser object to feed to the plugin
+        section: the section name in the config object that is relevant for this plugin
+        """
+        self.log.debug('Starting')
+        ko = self._getpluginclass(paths, name)
         po = ko(parent, config, section)
+        self.log.debug('Leaving, returning %s' %po)
         return po
     
         
-        
-    def _getpluginclass(self, level, kind, name):
+    def _getpluginclass(self, paths, name):
         '''
-        e.g. getpluginclass('queue','monitor','APF')
-        
-        returns plugin class. Classes, not objects. The __init__() methods have not been 
-        called yet.
+        returns a plugin class. 
+        The __init__() methods have not been called yet.
+        paths: list of subdirectories from where to import the plugin(s)
+        name: name of the single plugin to be imported
         '''
-        ppath = 'autopyfactory.plugins.%s.%s.%s'  %(level, kind, name)
+        self.log.debug('Starting')
+        ppath = '.'.join(paths)
+        ppath = ppath + '.' + name
+        
         try:
+            self.log.debug("trying to import %s from %s" %(name, ppath))
             plugin_module = __import__(ppath, globals(), locals(), name)
         except Exception, ex:
             self.log.error(ex)
     
         plugin_class = getattr(plugin_module, name)
-        self.log.trace("Retrieved plugin class %s" % name)
+        self.log.debug("Retrieved plugin with class name %s" % name)
         return plugin_class
-    
 
