af544794369798d184dc4007a44c36f2ea41646e
Jose Caballero <jcaballero@bnl.gov>
2017-02-24 14:09:05 -0500

    bug

--- autopyfactory/plugins/queue/wmsstatus/Condor.py
+++ autopyfactory/plugins/queue/wmsstatus/Condor.py
@@ -57,7 +57,7 @@ class _condor(_thread, WMSStatusInterface):
         self.remoteschedd = None
         self.remotecollector = None
         if self.queryargs:
-            l = queryargs.split()  # convert the string into a list
+            l = self.queryargs.split()  # convert the string into a list
             if '-name' in l:
                 self.remoteschedd = l[l.index('-name') + 1]
             if '-pool' in l:
