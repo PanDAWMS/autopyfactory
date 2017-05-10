54e24f9391ae99afd4140fe2af3ed459abdce74c
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-05-10 16:52:36 -0400

    Update.

--- autopyfactory/plugins/queue/batchsubmit/CondorEC2.py
+++ autopyfactory/plugins/queue/batchsubmit/CondorEC2.py
@@ -70,7 +70,7 @@ class CondorEC2(CondorGrid):
             super(CondorEC2, self).submit(num)
         else:
             self.log.debug("Checking for jobs in 'retiring' state...")
-            batchinfo = self.apfqueue.batchstatus_plugin.getInfo(queue = self.apfqueue.apfqname, maxtime = self.apfqueue.batchstatusmaxtime)
+            batchinfo = self.apfqueue.batchstatus_plugin.getInfo(queue = self.apfqueue.apfqname, maxtime = 360)
             numretiring = batchinfo.retiring
             self.log.debug("%d jobs in 'retiring' state." % numretiring)
             numleft = num - numretiring
