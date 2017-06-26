71e4d3326a17d4503bd556a86ecdeacdf71cb6d1
Jose Caballero <jcaballero@bnl.gov>
2017-06-26 15:13:48 -0400

    first draft code for auth config plugin File

--- autopyfactory/plugins/factory/config/auth/File.py
+++ autopyfactory/plugins/factory/config/auth/File.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import logging
-import os
 
 from autopyfactory.apfexceptions import ConfigFailure
 from autopyfactory.configloader import Config, ConfigManager
@@ -16,6 +15,13 @@ class File(ConfigInterface):
         self.factory = factory
         self.fcl = config
         self.qcl = None
-    
         self.log.info('ConfigPlugin: Object initialized.')
 
+
+    def getConfig(self):
+
+        acf = self.fcl.get('Factory', 'authConf')
+        self.log.debug('authmanager config file(s) = %s' %acf)
+        acl = ConfigManager().getConfig(sources=acf)
+        self.log.debug('successfully read config file(s) %s' %acf)
+        return acl
