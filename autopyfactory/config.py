32997a47369a51b35443e3811caf5181bd34977c
Jose Caballero <jcaballero@bnl.gov>
2017-07-16 13:12:21 -0400

    not attempting to reconfig authmanager if it is disabled

--- autopyfactory/config.py
+++ autopyfactory/config.py
@@ -23,6 +23,7 @@ class ConfigHandler(_thread):
 
         self.factory = factory
         self.reconfig = factory.fcl.generic_get('Factory', 'config.reconfig', 'getboolean', default_value=False)
+        self.authmanagerenabled = factory.fcl.generic_get('Factory', 'authmanager.enabled', 'getboolean', default_value=False)
         self.interval = None
         if self.reconfig:
             self.interval = factory.fcl.generic_get('Factory','config.reconfig.interval', 'getint', default_value=3600) 
@@ -32,6 +33,7 @@ class ConfigHandler(_thread):
 
 
     def setconfig(self):
+        self.log.debug('starting')
         # NOTE:
         # for now, we reconfig both or none
         if self.reconfig:
@@ -42,12 +44,15 @@ class ConfigHandler(_thread):
 
 
     def _startthread(self):
+        self.log.debug('starting')
         self.factory.threadsregistry.add("core", self)
         self._thread_loop_interval = self.interval
         self.start()
+        self.log.debug('leaving')
 
 
     def _run(self):
+        self.log.debug('starting')
         # order matters here: 
         # first reconfig AuthManager, then APFQueuesManager
         try:
@@ -59,29 +64,38 @@ class ConfigHandler(_thread):
             self._run_queues()
         except:
             self.log.error('seting configuration for queues failed.')
+        self.log.debug('leaving')
 
 
     def _run_auth(self):
-        self.acl = self.getAuthConfig()
-        self.factory.authmanager.reconfig(self.acl)
-        self.factory.authmanager.activate()
-        self.log.debug("Completed creation of %d auth handlers." % len(self.factory.authmanager.handlers))
+        self.log.debug('starting')
+        if self.authmanagerenabled:
+            self.log.debug('auth manager is enabled. Proceeding.')
+            self.acl = self.getAuthConfig()
+            self.factory.authmanager.reconfig(self.acl)
+            self.factory.authmanager.activate()
+            self.log.debug("Completed creation of %d auth handlers." % len(self.factory.authmanager.handlers))
+        self.log.debug('leaving')
 
 
     def _run_queues(self):
+        self.log.debug('starting')
         self.qcl = self.getQueuesConfig()
         self.factory.apfqueuesmanager.reconfig(self.qcl)
         self.factory.apfqueuesmanager.activate() #starts all threads
+        self.log.debug('leaving')
 
 
     def getAuthConfig(self):
         """
         get updated configuration from the Factory Config/Auth plugins
         """
+        self.log.debug('starting')
         newconfig = Config()
         for config_plugin in self.factory.auth_config_plugins:
             tmpconfig = config_plugin.getConfig()
             newconfig.merge(tmpconfig)
+        self.log.debug('leaving with newconf = %s' %newconfig)
         return newconfig
 
 
@@ -89,8 +103,10 @@ class ConfigHandler(_thread):
         """
         get updated configuration from the Factory Config/Queues plugins
         """
+        self.log.debug('starting')
         newconfig = Config()
         for config_plugin in self.factory.queues_config_plugins:
             tmpconfig = config_plugin.getConfig()
             newconfig.merge(tmpconfig)
+        self.log.debug('leaving with newconf = %s' %newconfig)
         return newconfig
