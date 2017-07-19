22af4c40e4da401c57e5a592f8e67119881487d1
root <root@apf.virtualclusters.org>
2017-07-19 20:59:12 +0000

    updated

--- autopyfactory/plugins/factory/config/queues/VC3.py
+++ autopyfactory/plugins/factory/config/queues/VC3.py
@@ -3,17 +3,14 @@
 import logging
 import os
 
-from ConfigParser import ConfigParser, SafeConfigParser
+from ConfigParser import SafeConfigParser
 from ConfigParser import NoOptionError
 
 from autopyfactory.apfexceptions import ConfigFailure
 from autopyfactory.configloader import Config, ConfigManager
 from autopyfactory.interfaces import ConfigInterface
 
-try:
-     from vc3 import client
-except ImportError:
-    pass
+from vc3client import client
 
 
 class VC3(ConfigInterface):
@@ -24,18 +21,14 @@ class VC3(ConfigInterface):
         self.fcl = factory.fcl
         self.log.info('ConfigPlugin: Object initialized.')
 
-        self.__fake()
+        conf = SafeConfigParser()
+	conf.readfp(open('/etc/vc3/vc3-client.conf'))
+	self.clientapi = client.VC3ClientAPI(conf)
     
-
-    def __fake(self):
-        config = None # I don't see in the VC3 code config being used 
-        clientapi = client.VC3ClientAPI(config)
-        users = clientapi.listUsers()
-        self.log.debug('users from InfoService = %s' %users)
-
     def getConfig(self):
-        cp = ConfigParser.ConfigParser()
         self.log.debug("Generating config object...")
-        return cp
+	requests = self.clientapi.listRequests()
+	self.log.debug('got list of Requests = %s' %requests)
         
-        
+
+
