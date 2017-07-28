5392dfffb4c85fd62b57c827ce3302d8f2fd2bba
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-07-28 14:40:49 -0400

    Dummy monitor infrastructure.

---
+++ autopyfactory/plugins/queue/monitor/Dummy.py
@@ -0,0 +1,37 @@
+
+import logging
+
+from autopyfactory.interfaces import MonitorInterface
+
+class Dummy(MonitorInterface):
+    
+    def __init__(self, apfqueue, config, section):
+        self.log = logging.getLogger("autopyfactory.monitor")
+        self.log.debug("Dummy monitor initialized.")
+
+
+    def registerFactory(self, apfqueue):
+        """
+        Initial startup hello message from new factory...
+        
+        """
+        self.log.debug("registerFactory() called.")
+        return None
+    
+    
+    def sendMessage(self, text):
+        """
+        Send message to monitor, if it supports this function. 
+        """
+        self.log.debug("sendMessage() called.")
+    
+    
+    def updateJobs(self, jobinfolist ):
+        """
+        Update information about job/jobs. 
+        Should support either single job object or list of job objects.  
+         
+        """
+        self.log.debug("updateJobs() called.")
+        return None
+   
