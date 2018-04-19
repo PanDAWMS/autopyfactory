cac0653517d8bf5757415fed4b32690185d36521
Jose Caballero <jcaballero@bnl.gov>
2018-04-18 20:49:30 -0400

    updated

--- autopyfactory/info2.py
+++ autopyfactory/info2.py
@@ -22,11 +22,12 @@ class StatusInfo(object):
     """
     """
 
-    def __init__(self, data, timestamp=None):
+    def __init__(self, data, is_raw=True, timestamp=None):
         """ 
         :param data: the initial set of data
         """ 
         self.log = logging.getLogger('autopyfactory')
+        self.is_raw = is_raw
         self.data = data 
         if not timestamp:
             self.timestamp = int(time.time())
@@ -38,19 +39,25 @@ class StatusInfo(object):
         """
         :param analyzer: an object implementing method aggregate()
         """
-        new_data = {} 
-        # assuming for now that data is list of dicts
+        #new_data = {} 
+        #for item in self.data:
+        #    key, value = analyzer.aggregate(item)
+        #    if key not in new_data.keys():
+        #        new_data[key] = []
+        #    new_data[key].append(value) 
+        #new_info = StatusInfo(new_data, False, self.timestamp)
+        #return new_info
+        tmp_new_data = {} 
         for item in self.data:
             key, value = analyzer.aggregate(item)
-            if key not in new_data.keys():
-                new_data[key] = []
-            new_data[key].append(value) 
-        new_info = StatusInfo(new_data, self.timestamp)
+            if key not in tmp_new_data.keys():
+                tmp_new_data[key] = []
+            tmp_new_data[key].append(value) 
+        new_data = {}
+        for k, v in tmp_new_data:
+            new_data[k] = StatusInfo(v, True, self.timestamp)
+        new_info = StatusInfo(new_data, False, self.timestamp)
         return new_info
-
-        # FIXME
-        # how to aggregate twice?
-        # how to aggregate when self.data is already a dict of lists of dicts?
 
 
     def modify(self, analyzer):
@@ -58,11 +65,10 @@ class StatusInfo(object):
         :param analyzer: an object implementing method modify()
         """
         new_data = []
-        # assuming for now that data is list of dicts
         for item in self.data:
             new_item = analyzer.modify(item)
             new_data.append(new_item)
-        new_info = StatusInfo(new_data, self.timestamp)
+        new_info = StatusInfo(new_data, True, self.timestamp)
         return new_info
 
 
@@ -71,10 +77,16 @@ class StatusInfo(object):
         :param analyzer: an object implementing method filter()
         """
         new_data = []
-        # assuming for now that data is list of dicts
         for item in self.data:
             if analyzer.filter(item):
                 new_data.append(item)
-        new_info = StatusInfo(new_data, self.timestamp)
+        new_info = StatusInfo(new_data, True, self.timestamp)
         return new_info
+
+
+    def get(self, *keys, process=None):
+        """
+        :param *keys: list of keys for each nested dictionary
+        :param process: a function that process the raw data, if needed.
+        """
 
