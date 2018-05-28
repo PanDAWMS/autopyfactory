e66b1e47040aef87e7f6de0adb816b140e980a72
Jose Caballero <jcaballero@bnl.gov>
2018-05-28 11:30:54 -0400

    updated

--- autopyfactory/plugins/queue/sched/MaxToSubmit.py
+++ autopyfactory/plugins/queue/sched/MaxToSubmit.py
@@ -7,6 +7,8 @@ from autopyfactory.interfaces import SchedInterface
 
 class MaxToSubmit(SchedInterface):
     """
+    Keep the number of jobs submitted during the whole history
+    of the APFQueue below some limit
     """
     id = 'maxtosubmit'
 
