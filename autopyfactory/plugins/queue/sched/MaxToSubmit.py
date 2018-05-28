484163242f1dc0af870ddb7019eed381f51663b1
Jose Caballero <jcaballero@bnl.gov>
2018-05-28 11:29:20 -0400

    added emtpy new plugins

---
+++ autopyfactory/plugins/queue/sched/MaxToSubmit.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#
+import logging
+
+from autopyfactory.interfaces import SchedInterface
+
+
+class MaxToSubmit(SchedInterface):
+    """
+    """
+    id = 'maxtosubmit'
+
+
+    # TO BE IMPLEMENTED
---
+++ autopyfactory/plugins/queue/sched/MinToSubmit.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#
+import logging
+
+from autopyfactory.interfaces import SchedInterface
+
+
+class MinToSubmit(SchedInterface):
+    """
+    """
+    id = 'mintosubmit'
+
+
+    # TO BE IMPLEMENTED
