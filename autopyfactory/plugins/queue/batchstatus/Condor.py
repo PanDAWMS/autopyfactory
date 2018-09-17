effb8d56258e8857d68b79b48cbdc9ef9b6c722f
Jose Caballero <jcaballero@bnl.gov>
2018-09-17 10:11:03 -0400

    bug fixed

--- autopyfactory/plugins/queue/batchstatus/Condor.py
+++ autopyfactory/plugins/queue/batchstatus/Condor.py
@@ -405,10 +405,10 @@ class Condor(object):
         #        else:
         #            id = '%s:%s' %(name, pool)
 
-        scheddhost = conf.generic_get(self.apfqname, 'batchstatus.condor.scheddhost', default_value='localhost')
-        scheddport = conf.generic_get(self.apfqname, 'batchstatus.condor.scheddport', default_value=9618 )
-        collectorhost = conf.generic_get(self.apfqname, 'batchstatus.condor.collectorhost', default_value='localhost')
-        collectorport = conf.generic_get(self.apfqname, 'batchstatus.condor.collectorport', default_value=9618 )
+        scheddhost = conf.generic_get(section, 'batchstatus.condor.scheddhost', default_value='localhost')
+        scheddport = conf.generic_get(section, 'batchstatus.condor.scheddport', default_value=9618 )
+        collectorhost = conf.generic_get(section, 'batchstatus.condor.collectorhost', default_value='localhost')
+        collectorport = conf.generic_get(section, 'batchstatus.condor.collectorport', default_value=9618 )
         id = '%s:%s:%s:%s' %(scheddhost, scheddport, collectorhost, collectorport)
 
         if not id in Condor.instances.keys():
